66e80c200b6f2ede3993561e28e40a26be53875b
Andreyrvs <cortaesai@gmail.com>
2021-10-13 14:21:51 -0300

    Atualiza

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-progamacao/dia-01-javascript-primeiros-passos/script-05.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-progamacao/dia-01-javascript-primeiros-passos/script-05.js
@@ -1,8 +1,14 @@
-const ladoA = 120;
-const ladoB = 100;
-const ladoC = 100;
+const ladoA = 10;
+const ladoB = 120;
+const ladoC = 120;
 
-if (ladoA / ladoB == ladoB / ladoC == ladoA / ladoC){
+let somaAB = ladoA, ladoB;
+let somaBC = ladoB, ladoC;
+let somaAC = ladoA, ladoC;
+
+console.log(somaAB)
+
+if (ladoA + ladoB + ladoC == 180 ){
   console.log("true")
 } else {
   console.log("false")
