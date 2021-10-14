e9eadda36111078ff61a494291ff38a448a6a848
Andreyrvs <cortaesai@gmail.com>
2021-10-14 15:42:15 -0300

    Exercicio - 06

---
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-progamacao/dia-02-javascript-array-e-loop-for/script-06.js
@@ -0,0 +1,11 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let numImpares;
+
+for (let index = 0; index < numbers.length; index++) {
+  if (numbers[index] % 2 == 0 ) {
+    console.log("É par")
+  } else {
+    console.log("È impar")
+  }
+  
+}
