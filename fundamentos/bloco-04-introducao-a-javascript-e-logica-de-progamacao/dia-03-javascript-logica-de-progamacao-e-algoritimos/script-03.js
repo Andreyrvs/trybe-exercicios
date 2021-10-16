77b56430e0bef0c5a56798cd33e1533fa979799e
Andreyrvs <cortaesai@gmail.com>
2021-10-16 20:00:25 -0300

    Exercicio - 03 incompleto

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-progamacao/dia-03-javascript-logica-de-progamacao-e-algoritimos/script-03.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-progamacao/dia-03-javascript-logica-de-progamacao-e-algoritimos/script-03.js
@@ -1,7 +1,7 @@
-let n = 5;
-let asterisco = '';
 
-for (let i = 0; i <= n; i++) {
-  asterisco += '*';
-  console.log(asterisco)
-}
+
+// console.log('    *')
+// console.log('   **')
+// console.log('  ***')
+// console.log(' ****')
+// console.log('*****')
