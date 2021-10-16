6ed24e2c88152d335cc7b55d0bb4c5f4128a2b2e
Andreyrvs <cortaesai@gmail.com>
2021-10-15 21:08:55 -0300

    Exercicio - 06

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-progamacao/dia-02-javascript-array-e-loop-for/script-06.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-progamacao/dia-02-javascript-array-e-loop-for/script-06.js
@@ -1,11 +1,11 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-let numImpares = numbers[0];
+let numOdd;
 
 for (let index = 0; index < numbers.length; index+= 1) {
   
-  if (numbers[index] % 2 == 0 ) {
-    console.log('É par')
-  } else {
-    console.log('È impar')
+  if (numbers[index] % 2 === 1 ) {
+    console.log(numbers[index] + ' È impar')
   } 
 }
+
+console.log('nenhum valor impar encontrado')
