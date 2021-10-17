ebffcf0a698cbef46c90138bebb2a7a8b0d37ad7
Andreyrvs <cortaesai@gmail.com>
2021-10-16 23:52:11 -0300

    Atualiza nomes de arquivos

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-progamacao/dia-04-javascript-objetos-e-funcoes/para-fixar/script-Funcoes-01.js
+++
@@ -1,7 +0,0 @@
-let personData = {
-  myName: 'Andrey',
-  birthCity: 'Paulo Frontin',
-  birthYear: 1997,
-}
-
-console.log('Meu nome é ' + personData.myName + ', eu nasci na cidade de ' + personData.birthCity + ' no ano de ' + personData.birthYear); 
