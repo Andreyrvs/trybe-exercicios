c7bc06819aa2897e75a140a88f6ccda8ea3f8bc6
Andreyrvs <cortaesai@gmail.com>
2021-10-17 13:28:23 -0300

    Para Fixar - Refatorar - Exericico 05

---
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-progamacao/dia-04-javascript-objetos-e-funcoes/para-fixar/script-Funcoes-05.js
@@ -0,0 +1,10 @@
+function triangleAngles(sideA, sideB, sideC) {
+  if (sideA + sideB + sideC === 180) {
+    console.log('true')
+  } else {
+    console.log('false')
+  }
+  return sideA, sideB,sideC
+}
+
+triangleAngles(50,30,100)
