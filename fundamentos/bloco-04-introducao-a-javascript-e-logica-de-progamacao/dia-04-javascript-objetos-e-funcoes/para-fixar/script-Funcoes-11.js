0673f27faefee089217bcf62872f0b0c07fc299c
Andreyrvs <cortaesai@gmail.com>
2021-10-17 18:43:59 -0300

    Para Fixar - Refatorar - Exercicio 11

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-progamacao/dia-04-javascript-objetos-e-funcoes/para-fixar/script-Funcoes-11.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-progamacao/dia-04-javascript-objetos-e-funcoes/para-fixar/script-Funcoes-11.js
@@ -30,10 +30,10 @@ function salaryWithoutTax(grossSalary) {
     portionIR27: 869.36,
   } 
   let aliquotIR = {
-    aliquotIR7: (baseSalary / 100 * 7.5) - portionIR.portionIR7,
-    aliquotIR15: (baseSalary / 100 * 15) - portionIR.portionIR15,
-    aliquotIR22: (baseSalary / 100 * 22.5) - portionIR.portionIR22,
-    aliquotIR27: (baseSalary / 100 * 27.5) - portionIR.portionIR27,
+    aliquot7PercentIR: (baseSalary / 100 * 7.5) - portionIR.portionIR7,
+    aliquot15PercentIR: (baseSalary / 100 * 15) - portionIR.portionIR15,
+    aliquot22PercentIR: (baseSalary / 100 * 22.5) - portionIR.portionIR22,
+    aliquot27PercentIR: (baseSalary / 100 * 27.5) - portionIR.portionIR27,
 
   } 
 
@@ -41,22 +41,22 @@ function salaryWithoutTax(grossSalary) {
     netSalary = baseSalary;
     console.log('R$' + netSalary + ' -> Isento de imposto de renda')
   } else if (baseSalary > 1903.99 && baseSalary <= 2826.65) {
-    netSalary = baseSalary - aliquotIR.aliquotIR7;
+    netSalary = baseSalary - aliquotIR.aliquot7PercentIR;
 
     console.log('R$' + netSalary)
   } else if (baseSalary > 2826.66 && baseSalary <= 3751.05) {
-    netSalary = baseSalary - aliquotIR.aliquotIR15;
+    netSalary = baseSalary - aliquotIR.aliquot15PercentIR;
 
     console.log('R$' + netSalary)
   } else if (baseSalary > 3751.06 && baseSalary <= 4664.68) {
-    netSalary = baseSalary - aliquotIR.aliquotIR22;
+    netSalary = baseSalary - aliquotIR.aliquot22PercentIR;
 
     console.log('R$' + netSalary)
   } else if (baseSalary > 4664.68) {
-    netSalary = baseSalary - aliquotIR.aliquotIR27;
+    netSalary = baseSalary - aliquotIR.aliquot27PercentIR;
     console.log('R$' + netSalary)
   }
   
 }
 
-salaryWithoutTax(3000)
+salaryWithoutTax(9000)
