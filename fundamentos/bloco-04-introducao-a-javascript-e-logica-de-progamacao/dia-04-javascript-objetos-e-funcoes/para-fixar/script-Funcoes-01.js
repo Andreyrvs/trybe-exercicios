bfdd2bc39b3c6f0fd4858c67a10fb5975d47316a
Andreyrvs <cortaesai@gmail.com>
2021-10-16 23:05:48 -0300

    Para Fixar - Refatorar 01

---
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-progamacao/dia-04-javascript-objetos-e-funcoes/para-fixar/script-Funcoes-01.js
@@ -0,0 +1,7 @@
+let personData = {
+  myName: 'Andrey',
+  birthCity: 'Paulo Frontin',
+  birthYear: 1997,
+}
+
+console.log('Meu nome é ' + personData.myName + ', eu nasci na cidade de ' + personData.birthCity + ' no ano de ' + personData.birthYear); 
