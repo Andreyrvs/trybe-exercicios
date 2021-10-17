934fa68947d1e9e5106801248864d305b2a07b64
Andreyrvs <cortaesai@gmail.com>
2021-10-17 00:13:09 -0300

    Para Fixar - Refatorar - Exericico 03

---
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-progamacao/dia-04-javascript-objetos-e-funcoes/para-fixar/script-Funcoes-03.js
@@ -0,0 +1,11 @@
+function greaterThanThree(a, b, c) {
+  if (a > b && a > c) {
+    console.log(a)
+  } else if (b > a && b > c) {
+    console.log(b)
+  } else if (c > a && c > b) {
+    console.log(c)    
+  }
+}
+
+greaterThanThree(3443, 472,2230)
