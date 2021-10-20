7f4d6a508f1ec5e185e6dc900c21057e3c03670b
Andreyrvs <cortaesai@gmail.com>
2021-10-19 23:55:05 -0300

    Exericico - 5 melhorado

--- fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-01-javascript-dom-e-seletores/scritpt.js
+++ fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-01-javascript-dom-e-seletores/scritpt.js
@@ -1,20 +1,31 @@
+// 01
 function tagId(tag) {
   document.getElementsByClassName(tag)[0].innerText = 'Me vejo Trabalhando com progamação em uma empresa brasileira talvez';
   console.log(tagId.length);
 }
 tagId('texto-paragrafo');
 
+// 02
 function greenSquare(trybeColor) {
   document.getElementsByClassName('main-content')[0].style.backgroundColor = trybeColor;
 }
 greenSquare('rgb(76,164,109');
 
+// 03
 function whiteSquare(bgColor) {
   document.getElementsByClassName(bgColor)[0].style.backgroundColor = 'white';
 }
 whiteSquare('center-content')
 
+// 04
 function fixTextH1(h1) {
   document.getElementsByClassName(h1)[0].innerText = 'Exericício 5.1 - JavaScript'
 }
 fixTextH1('title')
+
+// 05 Não consegui fazer =(
+function upperCaseofP(p) {
+  let text = document.getElementsByClassName(p)[0].innerText;
+  document.getElementsByClassName(p)[0].innerText = text.toUpperCase();
+}
+upperCaseofP('texto-paragrafo')
