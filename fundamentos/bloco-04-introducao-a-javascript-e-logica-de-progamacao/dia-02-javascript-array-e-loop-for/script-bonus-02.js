3ac2ac23c602e2cb25b8281e666e3e51092ddbe1
Andreyrvs <cortaesai@gmail.com>
2021-10-20 21:09:33 -0300

    Exercicio Bonus nao resolvido

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-progamacao/dia-02-javascript-array-e-loop-for/script-bonus-02.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-progamacao/dia-02-javascript-array-e-loop-for/script-bonus-02.js
@@ -1,12 +1,11 @@
 //Ordene o array numbers em ordem decrescente e imprima seus valores;
-let numbers = [45, 27, 57, 1330, 560, 800, 200, 70, 945, 54];
-
-for (let i = 1; i < numbers.length; i += 1) {
-  for (let secondI = 0; secondI < i; secondI += 1) {
-    if (numbers[i] < numbers[secondI]) {
-      let position = numbers[i];
-      numbers[i] = numbers[secondI];
-      numbers[secondI] = position;
+let numbers = [45, 27, 57, 1330, 560, 800, 200, 70, 945, 54]
+for (let index = 1; index < numbers.length; index += 1) {
+  for (let secondIndex = 0; secondIndex < index; secondIndex += 1) {
+    if (numbers[index] < numbers[secondIndex]) {
+      let position = numbers[index];
+      numbers[index] = numbers[secondIndex];
+      numbers[secondIndex] = position;
     }
   }
 }
