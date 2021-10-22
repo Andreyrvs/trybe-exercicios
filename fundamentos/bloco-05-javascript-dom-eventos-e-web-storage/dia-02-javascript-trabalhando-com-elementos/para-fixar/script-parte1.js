0c02d0ed3787039722822e44d6da3f8de160e93b
Andreyrvs <cortaesai@gmail.com>
2021-10-21 21:02:12 -0300

    Para Fixar - Parte 1 - Exercicio 02

--- fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-02-javascript-trabalhando-com-elementos/para-fixar/script-parte1.js
+++ fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-02-javascript-trabalhando-com-elementos/para-fixar/script-parte1.js
@@ -1,1 +1,12 @@
-console.log(document.querySelector('#elementoOndeVoceEsta'));
+// 01 Acesse o elemento elementoOndeVoceEsta .
+// console.log(document.querySelector('#elementoOndeVoceEsta'));
+
+// 02 Acesse pai a partir de elementoOndeVoceEsta e adicione uma color a ele.
+// function ondeEstou() {
+//   let aqui = document.querySelector('#elementoOndeVoceEsta').parentElement;
+//   aqui.style.backgroundColor = 'red';
+//   console.log(aqui);
+// }
+// ondeEstou()
+
+// 
