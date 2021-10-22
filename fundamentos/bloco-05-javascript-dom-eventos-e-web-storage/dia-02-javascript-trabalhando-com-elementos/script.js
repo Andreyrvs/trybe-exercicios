389b0955fefaaa3ef96c47034c0b2f85b6a87f39
Andreyrvs <cortaesai@gmail.com>
2021-10-22 10:20:18 -0300

    Exericico - 04

--- fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-02-javascript-trabalhando-com-elementos/script.js
+++ fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-02-javascript-trabalhando-com-elementos/script.js
@@ -2,9 +2,10 @@
 let textHone = ['Exercício 5.2 - JavaScript DOM'];
 let itemHone = document.querySelector('body');
 let bodyChild = textHone;
-let createElementHone = document.createElement('h1')
-createElementHone.innerText = bodyChild
-itemHone.appendChild(createElementHone)
+let createElementHone = document.createElement('h1');
+createElementHone.innerText = bodyChild;
+
+itemHone.appendChild(createElementHone);
 console.log(itemHone);
 
 // 02 Adicione a tag main com classe como filho da <body>
@@ -14,6 +15,7 @@ let mainItem = document.querySelector('body');
 let createMainElement = document.createElement('main');
 createMainElement.innerText = mainText
 createMainElement.className = 'main-content';
+
 mainItem.appendChild(createMainElement);
 
 // 03 dicione a tag section com a classe como filho da tag main
@@ -23,6 +25,16 @@ let sectionItem = document.querySelector('main');
 let createSectionElement = document.createElement('section');
 createSectionElement.innerText = sectionText;
 createSectionElement.className = 'center-content';
+
 sectionItem.appendChild(createSectionElement);
 
-//
+// 04 Adicione a tag p como filho do section
+let pText = 'eu sou a tag p filha da section'
+let pItem = document.querySelector('section');
+
+let createElementP = document.createElement('p');
+createElementP.innerText = pText;
+
+pItem.appendChild(createElementP);
+
+// 
