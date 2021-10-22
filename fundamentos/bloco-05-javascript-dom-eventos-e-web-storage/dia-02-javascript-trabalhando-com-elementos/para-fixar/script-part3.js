e2dd9d3e8e0a3908818939214dd28f00baa3fee1
Andreyrvs <cortaesai@gmail.com>
2021-10-21 23:07:30 -0300

    Para Fixar - Parte 3 - Exercicio 01

---
+++ fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-02-javascript-trabalhando-com-elementos/para-fixar/script-part3.js
@@ -0,0 +1,8 @@
+// let fatherUp = document.querySelectorAll('#paiDoPai');
+
+// for (let index = 0; index < fatherUp.length; index += 1) {
+//   let element = fatherUp[index];
+
+// }
+
+// console.log(fatherUp);
