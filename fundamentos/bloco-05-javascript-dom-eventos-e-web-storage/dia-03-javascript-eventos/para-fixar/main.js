cae5a119be16e2aa9c26fdcebceca1df0b15a61c
Andreyrvs <cortaesai@gmail.com>
2021-10-24 18:17:44 -0300

    Para Fixar - Parte 2 - Exericico 04

--- fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-03-javascript-eventos/para-fixar/main.js
+++ fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-03-javascript-eventos/para-fixar/main.js
@@ -32,6 +32,12 @@ input.addEventListener('change', test)
 // redirecione para alguma página;
 // 4.1. Que tal redirecionar para seu portifólio?
 
+function redirectToPortfolio() {
+  window.open('https://andreyrvs.github.io/')
+   
+}
+myWebpage.addEventListener('dblclick',  redirectToPortfolio);
+
 // 5. Crie uma função que, ao passar o mouse sobre 'Meu top 3 do Spotrybefy', altere
 // a cor do mesmo;
 
