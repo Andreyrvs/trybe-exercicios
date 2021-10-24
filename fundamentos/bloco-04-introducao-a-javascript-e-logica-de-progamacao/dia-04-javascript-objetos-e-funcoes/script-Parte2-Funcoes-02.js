19540a7e96044d43d0ed7ba359a3f5dcec582962
Andreyrvs <cortaesai@gmail.com>
2021-10-23 22:28:43 -0300

    Parte II - Exericico 02

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-progamacao/dia-04-javascript-objetos-e-funcoes/script-Parte2-Funcoes-02.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-progamacao/dia-04-javascript-objetos-e-funcoes/script-Parte2-Funcoes-02.js
@@ -1,13 +1,15 @@
 function highestNumber(array) {
   
   let biggerNumber = 0;
+  let saveIndex;
   for (let index = 0; index < array.length; index += 1) {
     
     if (array[index] > biggerNumber) {
-      biggerNumber = array[index]
+      biggerNumber = array[index];
+      saveIndex = index;
     }
   }
-  return biggerNumber;
+  console.log(saveIndex);
 }
 
 highestNumber([2, 3, 6, 7, 10, 1]);
