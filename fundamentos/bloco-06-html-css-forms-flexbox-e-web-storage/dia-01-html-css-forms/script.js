b525b2023b153fea2a1e82a8b5a7e1164e76f5e4
Andreyrvs <cortaesai@gmail.com>
2021-10-29 19:38:12 -0300

    Cria opitons dos estados de forma dinamica

---
+++ fundamentos/bloco-06-html-css-forms-flexbox-e-web-storage/dia-01-html-css-forms/script.js
@@ -0,0 +1,44 @@
+let btnPreventDefault = document.querySelector('#prevent-default');
+const todosEstados = document.querySelector('#option-estados');
+
+
+const estados = [
+  'Acre',
+  'Alagoas',
+  'Amapá',
+  'Amazonas',
+  'Bahia',
+  'Ceará',
+  'Distrito Federal',
+  'Espírito Santo',
+  'Goiás',
+  'Maranhão',
+  'Mato Grosso',
+  'Mato Grosso do Sul',
+  'Minas Gerais',
+  'Pará',
+  'Paraíba',
+  'Paraná',
+  'Pernambuco',
+  'Piauí',
+  'Rio de Janeiro',
+  'Rio Grande do Norte',
+  'Rio Grande do Sul',
+  'Rondônia',
+  'Roraima',
+  'Santa Catarina',
+  'São Paulo',
+  'Sergipe',
+  'Tocantins',
+];
+
+function geraLista() {
+  
+  const selecionaDrop = document.querySelector('#select-estado');
+  for (let index = 0; index < estados.length; index += 1) {
+    const criaOption = document.createElement('option')
+    criaOption.innerText = estados[index];
+    selecionaDrop.appendChild(criaOption);
+  }
+}
+geraLista();
