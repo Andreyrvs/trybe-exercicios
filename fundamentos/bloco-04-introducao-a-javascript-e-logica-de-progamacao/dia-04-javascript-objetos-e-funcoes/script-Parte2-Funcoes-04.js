50d6f1152149f6d03cddeb6d45fba7ee814bd774
Andreyrvs <cortaesai@gmail.com>
2021-10-31 22:26:17 -0300

    Parte II - Exericico - 04

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-progamacao/dia-04-javascript-objetos-e-funcoes/script-Parte2-Funcoes-04.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-progamacao/dia-04-javascript-objetos-e-funcoes/script-Parte2-Funcoes-04.js
@@ -1,14 +1,4 @@
 function largerInQuantity(array) {
-  let biggerName;
-  array.split(',');
   
-  for (let index = 0; index < split.length; index += 1) {
-    
-    if (split[index] < biggerName) {
-      biggerName = split[index];
- 
-    }
-  }
-  console.log(biggerName);
 }
 largerInQuantity(['José', 'Lucas', 'Nádia', 'Fernanda', 'Cairo', 'Joana']);
