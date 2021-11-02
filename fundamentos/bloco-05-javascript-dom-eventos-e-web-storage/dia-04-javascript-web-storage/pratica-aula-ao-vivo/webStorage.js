b89b0f269339d7847bf086049c21fda6fbb81bb3
Andreyrvs <cortaesai@gmail.com>
2021-11-02 20:05:05 -0300

    Pratica aula-ao-vivo - Adiciona estrutura para colocar no localStorage e Retornar

--- fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-04-javascript-web-storage/pratica-aula-ao-vivo/webStorage.js
+++ fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-04-javascript-web-storage/pratica-aula-ao-vivo/webStorage.js
@@ -6,18 +6,30 @@ let pessoa = {
 const sessaoPrincipal = document.getElementById('principal');
 const btnAdiciona = document.querySelector('.btnAdicionar');
 
-btnAdiciona.addEventListener('click', adicionaPessoa)
+btnAdiciona.addEventListener('click', adicionaPessoa);
 
-function criaElemento(objeto) {}
+function criaElemento(objeto) {
+  const novoElemento = document.createElement('p');
+  novoElemento.innerText = objeto.nome + ' - ' + objeto.idade;
+
+  sessaoPrincipal.appendChild(novoElemento);
+}
 
 function adicionaPessoa() {
   pessoa.nome = document.getElementById('nome').value;
   pessoa.idade = document.getElementById('idade').value;
 
-  const novoElemento = document.createElement('p');
-  novoElemento.innerText = pessoa.nome + ' - ' + pessoa.idade;
+  localStorage.setItem(pessoa.nome, JSON.stringify(pessoa));
 
-  sessaoPrincipal.appendChild(novoElemento);
+  criaElemento(pessoa);
 }
 
-window.onload = function () {};
+window.onload = function () {
+  if (localStorage.length > 0) {
+    for (let index = 0; index < localStorage.length; index += 1) {
+      let key = localStorage.key(index);
+      let objeto = JSON.parse(localStorage.getItem(key));
+      criaElemento(objeto);
+    }
+  }
+};
