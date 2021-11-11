4fa73df8a0ea458988dad5fbd5bcf5b1f5a88d6f
Andreyrvs <cortaesai@gmail.com>
2021-11-11 16:02:25 -0300

    Resolve diretorios

--- fundamentos/bloco-07-introduçao-a-javascript-ES6-e-testes-unitarios/dia-01-javascript-ES6-let-const-arrow-functions-e-template-literals/script-parte-1-2.js
+++ fundamentos/bloco-07-introduçao-a-javascript-ES6-e-testes-unitarios/dia-01-javascript-ES6-let-const-arrow-functions-e-template-literals/script-parte-1-2.js
@@ -4,4 +4,8 @@ const oddsAndEvens = [13, 3, 4, 10, 7, 2];
 
 // Seu código aqui.
 
-console.log(oddsAndEvens); // será necessário alterar essa linha 😉
+console.log(`Os números ${oddsAndEvens.sort((a, b) => a - b)} se encontram ordenados de forma crescente`);
+ // será necessário alterar essa linha 😉
+
+
+ // não sei pq funcionou mas funcionou
