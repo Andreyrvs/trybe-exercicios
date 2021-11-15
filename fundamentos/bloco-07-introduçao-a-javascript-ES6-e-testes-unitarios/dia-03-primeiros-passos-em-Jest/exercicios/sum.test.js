3b4d741312920e3a553ae8fca392c9c168433a02
Andreyrvs <cortaesai@gmail.com>
2021-11-15 11:19:05 -0300

    Feat: exercicio 1.4 & Fix: 1.3

--- fundamentos/bloco-07-introduçao-a-javascript-ES6-e-testes-unitarios/dia-03-primeiros-passos-em-Jest/exercicios/sum.test.js
+++ fundamentos/bloco-07-introduçao-a-javascript-ES6-e-testes-unitarios/dia-03-primeiros-passos-em-Jest/exercicios/sum.test.js
@@ -8,6 +8,13 @@ describe(' A função sum(a, b) retorna a soma do parâmetro a com o b', () => {
     expect(sum(0, 0)).toBe(0);
   });
   it('Teste se a função sum lança um erro quando os parâmetros são 4 e "5" (string 5)', () => {
-    expect(sum(4, '5')).toBe(4, 5)
-  })
+    expect(() => {
+      sum(4, '5');
+    }).toThrowError();
+  });
+  it('Teste se a mensagem de erro é "parameters must be numbers" quando realizar a chamada sum(4, "5")', () => {
+    expect(() => {
+      sum(4, '5');
+    }).toThrowError(new Error('parameters must be numbers'));
+  });
 });
