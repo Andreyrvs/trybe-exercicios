777e5c57d2394f766bfc6c38bb7c09ac94d60c6d
Andreyrvs <cortaesai@gmail.com>
2021-11-15 11:56:43 -0300

    Feat: exercicio 3.2

--- fundamentos/bloco-07-introduçao-a-javascript-ES6-e-testes-unitarios/dia-03-primeiros-passos-em-Jest/exercicios/ myFizzBuzz.js
+++ fundamentos/bloco-07-introduçao-a-javascript-ES6-e-testes-unitarios/dia-03-primeiros-passos-em-Jest/exercicios/ myFizzBuzz.js
@@ -10,5 +10,5 @@ module.exports = myFizzBuzz
 // implemente seus testes aqui
 
 console.log(
-myFizzBuzz(15)
+myFizzBuzz(3)
 );
--- fundamentos/bloco-07-introduçao-a-javascript-ES6-e-testes-unitarios/dia-03-primeiros-passos-em-Jest/exercicios/ myFizzBuzz.test.js
+++ fundamentos/bloco-07-introduçao-a-javascript-ES6-e-testes-unitarios/dia-03-primeiros-passos-em-Jest/exercicios/ myFizzBuzz.test.js
@@ -4,5 +4,7 @@ describe(' A função myFizzBuzz(num) recebe um número num e retorna "fizzbuzz"
   it('Faça uma chamada com um número divisível por 3 e 5 e verifique se o retorno é o esperado', () => {
     expect(myFizzBuzz(15)).toBe('fizzbuzz')
   });
-  
+  it('Faça uma chamada com um número divisível por 3 e verifique se o retorno é o esperado', () => {
+    expect(myFizzBuzz(3)).toBe('fizz')
+  });
 });
