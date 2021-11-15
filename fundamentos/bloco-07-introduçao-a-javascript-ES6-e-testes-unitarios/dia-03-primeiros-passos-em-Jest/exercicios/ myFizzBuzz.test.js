8808bf3b7b90cc89ec94466a08dae32ad00b7a0d
Andreyrvs <cortaesai@gmail.com>
2021-11-15 12:01:23 -0300

    Fix: it dos arquivos myRemove.test & myFizzBuzz.test

--- fundamentos/bloco-07-introduçao-a-javascript-ES6-e-testes-unitarios/dia-03-primeiros-passos-em-Jest/exercicios/ myFizzBuzz.test.js
+++ fundamentos/bloco-07-introduçao-a-javascript-ES6-e-testes-unitarios/dia-03-primeiros-passos-em-Jest/exercicios/ myFizzBuzz.test.js
@@ -1,13 +1,13 @@
 const myFizzBuzz = require('./ myFizzBuzz')
 
 describe(' A função myFizzBuzz(num) recebe um número num e retorna "fizzbuzz" se o número for divisível por 3 e 5 , retorna "fizz" se for divisível apenas por 3 , retorna "buzz" se divisível apenas por 5 , retorna o próprio número caso não seja divisível por 3 ou 5 e retorna false caso num não seja um número', () => {
-  it('Faça uma chamada com um número divisível por 3 e 5 e verifique se o retorno é o esperado', () => {
+  it('Faça uma chamada com um número divisível por 3 e 5 e verifique se o retorno é "fizzbuzz"', () => {
     expect(myFizzBuzz(15)).toBe('fizzbuzz')
   });
-  it('Faça uma chamada com um número divisível por 3 e verifique se o retorno é o esperado', () => {
+  it('Faça uma chamada com um número divisível por 3 e verifique se o retorno é "fizz"', () => {
     expect(myFizzBuzz(3)).toBe('fizz')
   });
-  it ('Faça uma chamada com um número divisível por 5 e verifique se o retorno é o esperado', () => {
+  it ('Faça uma chamada com um número divisível por 5 e verifique se o retorno é "buzz"', () => {
     expect(myFizzBuzz(10)).toBe('buzz')
   });
 });
