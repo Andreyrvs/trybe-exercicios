5b69dd96018005624331c07ea2eb4e554fc48207
Andreyrvs <cortaesai@gmail.com>
2021-11-15 13:36:08 -0300

    Fix: encodeDecode.test : formas diferentes de fazer

--- fundamentos/bloco-07-introduçao-a-javascript-ES6-e-testes-unitarios/dia-03-primeiros-passos-em-Jest/exercicios/encodeDecode.test.js
+++ fundamentos/bloco-07-introduçao-a-javascript-ES6-e-testes-unitarios/dia-03-primeiros-passos-em-Jest/exercicios/encodeDecode.test.js
@@ -1,9 +1,18 @@
 const {encode, decode} = require('./encodeDecode');
 
 describe('Para as funções encode e decode', () => {
-  it('Teste se "encode" e "decode" são funções', () => {
+  // Feito com Boolean
+/*   it('Teste se "encode" e "decode" são funções', () => {
     expect({ encode, decode }).toBeTruthy();
+  }); */
+  // Outra Forma de fazer, feito com typeof
+  it('Teste se "encode" e "decode" são funções', () => {
+    expect(typeof encode).toBe('function');
   });
+  it('Teste se "encode" e "decode" são funções', () => {
+    expect(typeof decode).toBe('function');
+  });
+
   it('Para a função encode teste se as vogais a, e, i, o, u são convertidas em 1, 2, 3, 4, 5', () => {
     expect(encode('a, e, i, o, u')).toBe('1, 2, 3, 4, 5');
   });
---
+++ fundamentos/bloco-07-introduçao-a-javascript-ES6-e-testes-unitarios/dia-03-primeiros-passos-em-Jest/exercicios/tech.js
@@ -0,0 +1,6 @@
+function techList (){
+
+};
+
+
+module.exports = techList;
