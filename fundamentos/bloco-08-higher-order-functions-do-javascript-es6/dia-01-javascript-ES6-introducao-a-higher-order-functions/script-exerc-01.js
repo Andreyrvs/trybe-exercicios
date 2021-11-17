809c42fee63dbdd6bf107d35039a706b5fe9513e
Andreyrvs <cortaesai@gmail.com>
2021-11-17 18:08:45 -0300

    Feat: exercicio 01 Estilo @Atanes

--- fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-01-javascript-ES6-introducao-a-higher-order-functions/script-exerc-01.js
+++ fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-01-javascript-ES6-introducao-a-higher-order-functions/script-exerc-01.js
@@ -1,21 +1,43 @@
-const criaEmail = (nameCom) => {
-  const objEmail = {
-    nomeCompleto: nameCom,
-    email: '@trybe.com',
-  } 
-  const EmailCompleto = `${objEmail.nomeCompleto}${objEmail.email}`
-  const espacoToUnderline = EmailCompleto.replace(/\s/ig, '_')
-  return espacoToUnderline
-}
+// const criaEmail = (nameCom) => {
+//   const objEmail = {
+//     nomeCompleto: nameCom,
+//     email: '@trybe.com',
+//   } 
+//   const EmailCompleto = `${objEmail.nomeCompleto}${objEmail.email}`
+//   const espacoToUnderline = EmailCompleto.replace(/\s/ig, '_')
+//   return espacoToUnderline
+// }
 
 
-const newEmployees = () => {
+// const newEmployees = () => {
+//   const employees = {
+//     id1: criaEmail('Pedro Guerra'), // Nome: Pedro Guerra -> Chame sua função passando o nome Pedro Guerra como parâmetro, substituindo as aspas
+//     id2: criaEmail('Luiza Drumend'), // Nome: Luiza Drumond -> Chame sua função passando o nome Luiza Drumond como parâmetro, substituindo as aspas
+//     id3: criaEmail('Carla Paiva'), // Nome: Carla Paiva -> Chame sua função passando o nome Carla Paiva como parâmetro, substituindo as aspas
+//   }
+//   return employees;
+// };
+
+// console.log(newEmployees());
+
+/** Estilo do @Atanes
+ * No return eu posso retornar um objeto, array direto sem salvar um variaveis 
+ */
+
+const dadosEmployee = (nomeCompleto) => {
+  const email = `${nomeCompleto.replace(' ', '_').toLowerCase()}@trybe.com`;
+  return {nome: nomeCompleto, email: email};
+};
+
+console.log(dadosEmployee('Felipe Castanheira'));
+
+const newEmployees = (callback) => {
   const employees = {
-    id1: criaEmail('Pedro Guerra'), // Nome: Pedro Guerra -> Chame sua função passando o nome Pedro Guerra como parâmetro, substituindo as aspas
-    id2: criaEmail('Luiza Drumend'), // Nome: Luiza Drumond -> Chame sua função passando o nome Luiza Drumond como parâmetro, substituindo as aspas
-    id3: criaEmail('Carla Paiva'), // Nome: Carla Paiva -> Chame sua função passando o nome Carla Paiva como parâmetro, substituindo as aspas
+    id1: callback('Pedro Guerra'), // Nome: Pedro Guerra -> Chame sua função passando o nome Pedro Guerra como parâmetro, substituindo as aspas
+    id2: callback('Luiza Drumend'), // Nome: Luiza Drumond -> Chame sua função passando o nome Luiza Drumond como parâmetro, substituindo as aspas
+    id3: callback('Carla Paiva'), // Nome: Carla Paiva -> Chame sua função passando o nome Carla Paiva como parâmetro, substituindo as aspas
   }
   return employees;
 };
 
-console.log(newEmployees());
+console.log(newEmployees(dadosEmployee));
