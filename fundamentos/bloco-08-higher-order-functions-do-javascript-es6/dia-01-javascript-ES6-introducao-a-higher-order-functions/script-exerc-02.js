e4fe21ba44efb58380f568882e5a71d5d1755781
Andreyrvs <cortaesai@gmail.com>
2021-11-17 18:26:25 -0300

    Feat: exercicio 02 tentativa

--- fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-01-javascript-ES6-introducao-a-higher-order-functions/script-exerc-02.js
+++ fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-01-javascript-ES6-introducao-a-higher-order-functions/script-exerc-02.js
@@ -1,17 +1,34 @@
-const sorteio = (aposta) => {
-  
+// const sorteio = (aposta) => {
+
+//   const min = 1;
+//   const max = 5;
+//   const resultado = Math.floor(Math.random() * (max - min + 1) + min);
+//   return resultado
+// }
+
+// const checar = (callback) => {
+//   callback()
+//   if (resultado !== aposta) {
+//     return 'Tente Novamente'
+//   }
+//   return 'Parabéns você ganhou'
+// }
+
+// console.log(sorteio(checar(2)));
+
+const sorteio = (callback) => {
   const min = 1;
   const max = 5;
   const resultado = Math.floor(Math.random() * (max - min + 1) + min);
-  return resultado
-}
 
-const checar = () => {
-  if (resultado !== aposta) {
-    return 'Tente Novamente'
-  }
-  return 'Parabéns você ganhou'
-}
+  callback(resultado)
 
-console.log(sorteio(checar(2)));
+};
 
+const checar = (aposta) => {
+  if (aposta !== resultado) {
+    return 'Tente novamente'
+  } return 'Parabéns você ganhou'
+};
+
+console.log(sorteio(checar));
