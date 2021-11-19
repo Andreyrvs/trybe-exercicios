4b6afa253c5e4f2d8cb71c6cec14208bbdd37d49
Andreyrvs <cortaesai@gmail.com>
2021-11-19 09:12:13 -0300

    Fix: para-fixar - Array.find - 01

--- fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-02-javascript-ES6-higher-order-functions-forEach-find-same-every-sort/para-fixar/script-find.js
+++ fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-02-javascript-ES6-higher-order-functions-forEach-find-same-every-sort/para-fixar/script-find.js
@@ -1,12 +1,17 @@
+// ---------- Exercicio 01 ----------
+
 const numbers = [19, 21, 30, 3, 45, 22, 15];
 
 const findDivisibleBy3And5 = (numbers) => {
   // Adiciona seu código aqui
   if (numbers % 3 === 0 && numbers % 3 === 0) {
-    console.log(numbers)
+    return numbers
   }
 }
 
-numbers.find(findDivisibleBy3And5);
+const achaNumber = numbers.find(findDivisibleBy3And5);
 
-// console.log(findDivisibleBy3And5())
+console.log(findDivisibleBy3And5(achaNumber))
+
+
+// ---------- Exercicio 02 ----------
