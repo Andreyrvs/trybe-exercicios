0ca16c2d0edb529e76ceb3aa572aa551e61ad563
Andreyrvs <cortaesai@gmail.com>
2021-11-19 15:55:58 -0300

    Feat: exercicio 01

--- fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-03-javascript-ES6-higher-order-functions-map-filter/exercicio-01.js
+++ fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-03-javascript-ES6-higher-order-functions-map-filter/exercicio-01.js
@@ -1,2 +1,22 @@
 const books = require('./referencia');
 
+const expectedResult = [
+  'As Crônicas de Gelo e Fogo - Fantasia - George R. R. Martin',
+  'O Senhor dos Anéis - Fantasia - J. R. R. Tolkien',
+  'Fundação - Ficção Científica - Isaac Asimov',
+  'Duna - Ficção Científica - Frank Herbert',
+  'A Coisa - Terror - Stephen King',
+  'O Chamado de Cthulhu - Terror - H. P. Lovecraft',
+];
+
+function formatedBookNames() {
+  // escreva seu código aqui
+  // books.name
+  // books.genre
+  // books.author.name
+
+  return books.map((el) => `${el.name} - ${el.genre} - ${el.author.name}`);
+
+}
+
+console.log(formatedBookNames());
