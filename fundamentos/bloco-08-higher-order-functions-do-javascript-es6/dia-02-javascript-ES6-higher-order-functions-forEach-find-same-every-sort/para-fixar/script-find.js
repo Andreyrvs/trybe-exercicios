78a1a18c8f7cd71ee2eca1eb1b96cb639c919d10
Andreyrvs <cortaesai@gmail.com>
2021-11-19 09:43:00 -0300

    Feat: para-fixar - Array.find - 2.3

--- fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-02-javascript-ES6-higher-order-functions-forEach-find-same-every-sort/para-fixar/script-find.js
+++ fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-02-javascript-ES6-higher-order-functions-forEach-find-same-every-sort/para-fixar/script-find.js
@@ -3,27 +3,40 @@
 const numbers = [19, 21, 30, 3, 45, 22, 15];
 
 const findDivisibleBy3And5 = (numbers) => {
-  // Adiciona seu código aqui
   if (numbers % 3 === 0 && numbers % 3 === 0) {
-    return numbers
+    return numbers;
   }
-}
+};
 
 const achaNumber = numbers.find(findDivisibleBy3And5);
 
 // console.log(findDivisibleBy3And5(achaNumber))
 
-
 // ---------- Exercicio 02 ----------
 
 const names = ['João', 'Irene', 'Fernando', 'Maria'];
 
-const findNameWithFiveLetters = (letter) => {
-  // Adicione seu código aqui:
-  return letter.length === 5
+const findNameWithFiveLetters = (letter) => letter.length === 5;
+
+const achaName = names.find(findNameWithFiveLetters);
+
+// console.log(findNameWithFiveLetters(achaName));
+// console.log(achaName);
+
+// ---------- Exercicio 03 ----------
+
+const musicas = [
+  { id: '31031685', title: 'Partita in C moll BWV 997' },
+  { id: '31031686', title: 'Toccata and Fugue, BWV 565' },
+  { id: '31031687', title: 'Chaconne, Partita No. 2 BWV 1004' },
+];
+
+function findMusic(id) {
+ 
 }
 
-const achaName = names.find(findNameWithFiveLetters)
+const achaId = musicas.find((element) => element.id === '31031685')
+  
+console.log(achaId);
 
-console.log(findNameWithFiveLetters(achaName));
-console.log(achaName);
+// console.log(findMusic('31031685'));
