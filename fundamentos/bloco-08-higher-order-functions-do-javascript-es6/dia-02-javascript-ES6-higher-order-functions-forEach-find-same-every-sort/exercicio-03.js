0d06dcea3b05bd19496cd7020dbe2d2c10451f3d
Andreyrvs <cortaesai@gmail.com>
2021-11-19 11:10:00 -0300

    Feat: Exercicio - 03

---
+++ fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-02-javascript-ES6-higher-order-functions-forEach-find-same-every-sort/exercicio-03.js
@@ -0,0 +1,19 @@
+const books = require('./referencia');
+
+const expectedResult = {
+  author: {
+    birthYear: 1948,
+    name: 'George R. R. Martin',
+  },
+  genre: 'Fantasia',
+  id: 1,
+  name: 'As Crônicas de Gelo e Fogo',
+  releaseYear: 1991,
+};
+
+function getNamedBook() {
+  // escreva seu código aqui
+  return books.find((book) => book.name.length === 26)
+}
+
+console.log(getNamedBook());
