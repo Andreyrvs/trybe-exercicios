937f93c232dbaa3c3a45520d28bed9b63414913f
Andreyrvs <cortaesai@gmail.com>
2021-11-19 11:30:30 -0300

    Fix: Exercicio - 05

--- fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-02-javascript-ES6-higher-order-functions-forEach-find-same-every-sort/exericico-05.js
+++ fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-02-javascript-ES6-higher-order-functions-forEach-find-same-every-sort/exericico-05.js
@@ -1,14 +1,11 @@
-const books = require('./referencia')
+const books = require('./referencia');
 
 const expectedResult = false;
 
 function everyoneWasBornOnSecXX() {
-  // escreva seu código aqui
-
-  return books.every((born) => {
-    born.author.birthYear >= 1901 && born.author.birthYear <= 2000
-  })
-
+  return books.every(
+    (born) => born.author.birthYear >= 1901 && born.author.birthYear <= 2000
+  );
 }
 
 console.log(everyoneWasBornOnSecXX());
