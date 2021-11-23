0c3750601f8d5579c60c42645c9c8acb69a9e774
Andreyrvs <cortaesai@gmail.com>
2021-11-23 10:34:27 -0300

    Feat: exercicio 03 auxilio Gabarito

--- fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-04-javascript-ES6-higher-order-functions-reduce/exercicio-03.js
+++ fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-04-javascript-ES6-higher-order-functions-reduce/exercicio-03.js
@@ -1,16 +1,17 @@
 const books = require('./reference-02-03-04');
-const assert = require('assert')
+const assert = require('assert');
 
 const expectedResult = 43;
 
 // 3 - Calcule a média de idade que as pessoas autoras tinham quando seus respectivos livros foram lançados.
 function averageAge() {
   // escreva seu código aqui
-  let tamanho = books.length
-  const livro = books.reduce((acumulador, valorAtual) => acumulador.releaseYear - valorAtual.author.birthYear, 0)
-  return livro / tamanho
+  let numeroDeLivros = books.length;
+  const mediaDasIdades = books.reduce(
+    (acumulador, valorAtual) => acumulador + (valorAtual.releaseYear - valorAtual.author.birthYear), 0
+  );
+  return mediaDasIdades / numeroDeLivros;
 }
 
-
-console.log(averageAge())
-// assert.deepStrictEqual(averageAge(), expectedResult)
+console.log(averageAge());
+assert.deepStrictEqual(averageAge(), expectedResult)
