aaee8a0ab125e5132153a42075f67e65b5325777
Andreyrvs <cortaesai@gmail.com>
2021-11-25 20:56:38 -0300

    Feat: para-fixar - default_parameters

--- fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-05-javascript-ES6-opread-operator-parametro-rest-destructuring-e-mais/para-fixar/Default_Parameters .js
+++ fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-05-javascript-ES6-opread-operator-parametro-rest-destructuring-e-mais/para-fixar/Default_Parameters .js
@@ -1,8 +1,9 @@
 // Para Fixar
 // Para praticar, escreva uma função multiply que multiplique dois números passados como argumentos. Atribua como default o valor 1 caso não seja passado nenhum valor como segundo parâmetro.
 
-const multiply = (number, value) => {
+const multiply = (number, value = 1) => {
   // Escreva aqui a sua função
+  return number * value
 };
 
 console.log(multiply(8));
