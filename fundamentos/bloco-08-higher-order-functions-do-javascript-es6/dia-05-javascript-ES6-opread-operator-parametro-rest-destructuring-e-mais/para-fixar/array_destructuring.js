839ff33b17c9f3f5b69481201e5aac7858f0e620
Andreyrvs <cortaesai@gmail.com>
2021-11-25 20:35:39 -0300

    Fix: para-fixar - array-destructuring 02 auxilio gabarito

--- fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-05-javascript-ES6-opread-operator-parametro-rest-destructuring-e-mais/para-fixar/array_destructuring.js
+++ fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-05-javascript-ES6-opread-operator-parametro-rest-destructuring-e-mais/para-fixar/array_destructuring.js
@@ -10,6 +10,7 @@ const [saudocao, realizaSaudacao] = saudacoes
 
 console.log(realizaSaudacao(saudocao));
 
+
 // 2 - A seguir, temos alguns valores que estão descritos em variáveis incoerentes. Através da desestruturação de arrays, corrija os valores das variáveis.
 
 let comida = 'gato';
@@ -20,6 +21,8 @@ console.log(comida, animal, bebida); // arroz gato água
 
 // Utilizando array destructuring, faça com que os valores apareçam nas variáveis correspondentes ao seu verdadeiro tipo
 
+[comida, animal, bebida] = ['arroz', 'gato', 'água']
+console.log(comida, animal, bebida);
 
 // 3 - array abaixo possui alguns números que não condizem com o conteúdo que ele deveria possuir. Através de array destructuring, faça com que existam apenas números pares na variável numerosPares .
 
