9a66a3bd20023fa1159c0cfdf6f80dfa35f6acfb
Andreyrvs <cortaesai@gmail.com>
2021-11-25 20:53:03 -0300

    Feat: para-fixar - default_destructuring

--- fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-05-javascript-ES6-opread-operator-parametro-rest-destructuring-e-mais/para-fixar/default_destructuring.js
+++ fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-05-javascript-ES6-opread-operator-parametro-rest-destructuring-e-mais/para-fixar/default_destructuring.js
@@ -1,28 +1,7 @@
-const person = {
-  name: 'João',
-  lastName: 'Jr',
-  age: 34,
-};
-
-const { nationality } = person;
-
-// ------------------
-const person = {
-  name: 'João',
-  lastName: 'Jr',
-  age: 34,
-};
-
-const { nationality = 'Brazilian' } = person;
-// console.log(nationality); // Brazilian
-
-
-// -----------------------------------
-
 // Para Fixar
 // Do jeito que está, quando passamos person para a função GetNationality o retorno é João is undefined . Ajuste a função GetNationality para que a chamada GetNationality(person) retorne João is Brazilian .
 
-const getNationality = ({ firstName, nationality }) => `${firstName} is ${nationality}`;
+const getNationality = ({ firstName, nationality = 'Brazilian' }) => `${firstName} is ${nationality}`;
 
 const person = {
   firstName: 'João',
@@ -34,8 +13,6 @@ const otherPerson = {
   lastName: 'Ivanovich',
   nationality: 'Russian',
 };
-
-const {nationality = 'Brazilian'} = person
 
 console.log(getNationality(otherPerson)); // Ivan is Russian
 console.log(getNationality(person));
