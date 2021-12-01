c40545208cb50c3b1e4245a7934bd2e1c849ac2c
Andreyrvs <cortaesai@gmail.com>
2021-12-01 14:45:39 -0300

    Feat: para-fixar async/await

--- fundamentos/bloco-09-javascript-e-testes-assincronos/dia-03-jest-testes-assincronos/para-fixar/getData.spec.js
+++ fundamentos/bloco-09-javascript-e-testes-assincronos/dia-03-jest-testes-assincronos/para-fixar/getData.spec.js
@@ -2,5 +2,15 @@ const getSuperHero = require('./getData');
 
 test('Verifica se o nome do herói é Wonder Woman', async () => {
   const hero = await getSuperHero();
-  expect(hero).toBe('Wonder Woman');
+  expect(hero)
+  .toBe('Wonder Woman');
+});
+test('Testa se é um falso positivo com asysc/await', async () => {
+  const hero = await getSuperHero();
+  expect(hero)
+  .toMatch('erro');
+});
+test('Testa se o retorno da requisição com url alterada, retorna o erro, com async/await', async () => {
+  const error = await getSuperHero();
+  expect(error).toMatch('erro');
 });
