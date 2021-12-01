b83f869cf5e37f2ec6b9c4a14a1a8606be5777f8
Andreyrvs <cortaesai@gmail.com>
2021-11-30 21:05:43 -0300

    Feat: exercicio 02 Refatora com auxilo gabarito

--- fundamentos/bloco-09-javascript-e-testes-assincronos/dia-02-javascript-assincrono-fetch-API-e-async-await/exercicio_02/api.js
+++ fundamentos/bloco-09-javascript-e-testes-assincronos/dia-02-javascript-assincrono-fetch-API-e-async-await/exercicio_02/api.js
@@ -1,31 +1,37 @@
 const API_URL = 'https://api.coincap.io/v2/assets';
 
-const fetchCripto = () => {
+const fetchCripto = async () => {
   const myObject = {
     method: 'GET',
     headers: { Accept: 'Accept-Encoding' },
   };
 
-  fetch(API_URL, myObject)
+  const coins = await fetch(API_URL, myObject)
     .then((response) => response.json())
-    .then((obj) =>
-      obj.data.map((datas) => datas)
-      .find((el) => append(`${el.name} (${el.symbol}): ${el.priceUsd}`))
-    );
+    .then((obj) => obj.data)
+    .catch((error) => error.toString())
 
-
+  return coins
 };
 
-function isBigEnough(value) {
-  return value >= 10
+// Filtra as 10 primeiras Modedas
+const filtraCoins = async () => {
+  const coins = await fetchCripto();
+
+  coins
+    .filter((coin) => Number(coin.rank) <= 10)
+    .forEach((coin) => {
+      append(`${coin.name} (${coin.symbol}): ${coin.priceUsd}`)
+    })
+
 }
 
+// Cria as li, é passada como na função fintraCoins
 function append(lista) {
   const catchH2 = document.querySelector('#listaCripto');
   const creatLi = document.createElement('li')
   creatLi.innerHTML = lista;
   catchH2.appendChild(creatLi)
 }
-fetchCripto()
-// window.onload = () => fetchCripto();
-//
+
+window.onload = () => filtraCoins();
