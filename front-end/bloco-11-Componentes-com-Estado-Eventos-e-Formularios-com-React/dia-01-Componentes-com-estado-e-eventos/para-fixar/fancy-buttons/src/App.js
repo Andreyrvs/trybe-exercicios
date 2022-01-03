7b4d623f6025ca048955898506d8180898a42ffb
Andreyrvs <cortaesai@gmail.com>
2022-01-03 16:29:51 -0300

    Feat: estado e eventos

--- front-end/bloco-11-Componentes-com-Estado-Eventos-e-Formularios-com-React/dia-01-Componentes-com-estado-e-eventos/para-fixar/fancy-buttons/src/App.js
+++ front-end/bloco-11-Componentes-com-Estado-Eventos-e-Formularios-com-React/dia-01-Componentes-com-estado-e-eventos/para-fixar/fancy-buttons/src/App.js
@@ -1,35 +1,46 @@
 import React from "react";
-import "./App.css";
 
-class App extends React.Component {
+class Test extends React.Component {
   constructor() {
     super();
+    this.state = {
+      btn1: 0,
+      btn2: 0,
+      btn3: 0,
+    };
+
     this.handleClick = this.handleClick.bind(this);
-    this.handleClick = this.handleClick2.bind(this);
-    this.handleClick = this.handleClick3.bind(this);
+    this.handleClick2 = this.handleClick2.bind(this);
+    this.handleClick3 = this.handleClick3.bind(this);
   }
 
-  handleClick() {
-    console.log("Clicou no botão 1!");
-  }
+  handleClick = () => {
+    this.setState((estadoAnteriror, _props) => ({
+      btn1: estadoAnteriror.btn1 + 1,
+    }));
+  };
 
-  handleClick2() {
-    console.log("Clicou no botão 2!!");
-  }
+  handleClick2 = () => {
+    this.setState((estadoAnteriror, _props) => ({
+      btn2: estadoAnteriror.btn2 + 1,
+    }));
+  };
 
-  handleClick3() {
-    console.log("Clicou no botão 3!!");
-  }
+  handleClick3 = () => {
+    this.setState((estadoAnteriror, _props) => ({
+      btn3: estadoAnteriror.btn3 + 1,
+    }));
+  };
 
   render() {
     return (
       <>
-        <button onClick={this.handleClick}> botão 1</button>
-        <button onClick={this.handleClick2}> botão 2</button>
-        <button onClick={this.handleClick3}> botão 3</button>
+        <button onClick={this.handleClick}>{this.state.btn1}</button>
+        <button onClick={this.handleClick2}>{this.state.btn2}</button>
+        <button onClick={this.handleClick3}>{this.state.btn3}</button>
       </>
     );
   }
 }
 
-export default App;
+export default Test;
