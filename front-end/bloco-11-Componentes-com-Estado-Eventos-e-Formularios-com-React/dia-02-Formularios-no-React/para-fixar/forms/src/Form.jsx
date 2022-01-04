dc3dfed455715e08ed6ce93c577ba5e004fb2907
Andreyrvs <cortaesai@gmail.com>
2022-01-04 18:18:54 -0300

    Feat: Para Fixar - event handlers genericos

--- front-end/bloco-11-Componentes-com-Estado-Eventos-e-Formularios-com-React/dia-02-Formularios-no-React/para-fixar/forms/src/Form.jsx
+++ front-end/bloco-11-Componentes-com-Estado-Eventos-e-Formularios-com-React/dia-02-Formularios-no-React/para-fixar/forms/src/Form.jsx
@@ -1,85 +1,95 @@
 import React, { Component } from "react";
-import './Form.css'
+import "./Form.css";
 
 class Form extends Component {
   constructor() {
     super();
 
-    this.handleChange = this.handleTextarea.bind(this);
-    this.handleIptNumber = this.handleIptNumber.bind(this);
-    this.handleCheckbox = this.handleCheckbox.bind(this);
-    this.handleSelect = this.handleSelect.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+
+    // this.handleIptNumber = this.handleIptNumber.bind(this);
+    // this.handleCheckbox = this.handleCheckbox.bind(this);
+    // this.handleSelect = this.handleSelect.bind(this);
 
     this.state = {
       estadoFavorito: "",
-      inputNumber: "",
-      inputCheckbox: "",
+      idade: "",
+      vaiComparecer: "",
       select: "maca",
     };
   }
 
-  handleTextarea(event) {
-    this.setState({
-      estadoFavorito: event.target.value,
-    });
-  }
-
-  handleIptNumber(event) {
-    this.setState({
-      inputNumber: event.target.value,
-    });
-  }
-
-  handleCheckbox(event) {
-    const target = event.target;
+  handleChange({ target }) {
+    const { name } = target;
     const value = target.type === "checkbox" ? target.checked : target.value;
+
     this.setState({
-      inputCheckbox: value,
+      [name]: value,
     });
   }
 
-  handleSelect(event) {
-    this.setState({
-      select: event.target.value,
-    });
-  }
+  // handleIptNumber(event) {
+  //   this.setState({
+  //     inputNumber: event.target.value,
+  //   });
+  // }
+
+  // handleCheckbox(event) {
+  //   const target = event.target;
+  //   const value = target.type === "checkbox" ? target.checked : target.value;
+  //   this.setState({
+  //     inputCheckbox: value,
+  //   });
+  // }
+
+  // handleSelect(event) {
+  //   this.setState({
+  //     select: event.target.value,
+  //   });
+  // }
 
   render() {
     return (
-      <div className='container'>
+      <div className="container">
         <h1>
           Estados e React - Tecnologia fantástica ou reagindo a regionalismos?
         </h1>
+
         <form className="form-container">
-          <label>
-            Diga qual o seu Estado favorito! De React ou do Brasil, você decide!
-            =)
-          </label>
+          <fieldset>
+            <label>
+              Diga qual o seu Estado favorito! De React ou do Brasil, você
+              decide! =)
+            </label>
             <textarea
               name="estadoFavorito"
               value={this.state.estadoFavorito}
-              onChange={this.handleTextarea}
+              onChange={this.handleChange}
             />
+
             <label>number</label>
-          <input
-            type="number"
-            name="idade"
-            value={this.state.inputNumber}
-            onChange={this.handleIptNumber}
-          />
-          <label>checkbox</label>
-          <input
-            type="checkbox"
-            name="vaiComparecer"
-            checked={this.state.inputCheckbox}
-            onChange={this.handleCheckbox}
-          />
-          <label>select</label>
-          <select value={this.state.select} onChange={this.handleSelect}>
-            <option value="maca">Maca</option>
-            <option value="banana">Banana</option>
-            <option value="pessego">Pessego</option>
-          </select>
+            <input
+              type="number"
+              name="idade"
+              value={this.state.idade}
+              onChange={this.handleChange}
+            />
+
+            <label>checkbox</label>
+            <input
+              type="checkbox"
+              name="vaiComparecer"
+              checked={this.state.vaiComparecer}
+              onChange={this.handleChange}
+            />
+
+            <label>select</label>
+            <select value={this.state.select} onChange={this.handleChange}>
+              <option value="maca">Maca</option>
+              <option value="banana">Banana</option>
+              <option value="pessego">Pessego</option>
+            </select>
+          </fieldset>
         </form>
       </div>
     );
