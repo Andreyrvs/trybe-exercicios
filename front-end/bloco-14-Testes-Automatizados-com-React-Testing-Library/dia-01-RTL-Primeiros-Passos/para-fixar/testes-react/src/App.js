f0b888eeebd943fbe1d89463a0fa2dc4208dfd67
Andreyrvs <cortaesai@gmail.com>
2022-02-04 19:04:16 -0300

    Dia 1

--- front-end/bloco-14-Testes-Automatizados-com-React-Testing-Library/dia-01-RTL-Primeiros-Passos/para-fixar/testes-react/src/App.js
+++ front-end/bloco-14-Testes-Automatizados-com-React-Testing-Library/dia-01-RTL-Primeiros-Passos/para-fixar/testes-react/src/App.js
@@ -9,6 +9,8 @@ function App() {
         Email
         <input id="id-email" type="email" />
       </label>
+      <input id="btn-send" type="button" value="Enviar" data-testid="id-send"/>
+      <input id="btn-back" type="button" value="Voltar" />
     </div>
   );
 }
--- front-end/bloco-14-Testes-Automatizados-com-React-Testing-Library/dia-01-RTL-Primeiros-Passos/para-fixar/testes-react/src/App.test.js
+++ front-end/bloco-14-Testes-Automatizados-com-React-Testing-Library/dia-01-RTL-Primeiros-Passos/para-fixar/testes-react/src/App.test.js
@@ -8,3 +8,17 @@ test('Verificando se existe o campo Email.', () => {
   expect(inputEmail).toBeInTheDocument();
   expect(inputEmail).toHaveProperty('type', 'email');
 });
+
+test('Verificando se existe dois botões', () => {
+  render(<App />);
+  const buttons = screen.getAllByRole('button');
+  expect(buttons).toHaveLength(2);
+});
+
+test('Verificando se existe um botão enviar', () => {
+  render(<App />);
+  const btnSend = screen.getByTestId('id-send');
+  expect(btnSend).toBeInTheDocument();
+  expect(btnSend).toHaveProperty('type', 'button');
+  expect(btnSend).toHaveValue('Enviar');
+});
