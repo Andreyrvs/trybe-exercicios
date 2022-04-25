680f305b2c1a8499cf09ea562ee92e12ad591dfd
Andreyrvs <cortaesai@gmail.com>
2022-04-25 12:47:45 -0300

    Exercices 5

--- back-end/bloco-22-Introducao-ao-desenvolvimento-Web-com-NodeJS/dia-01-NodeJS-Um-Motor-JavaScript/exercices/imc.js
+++ back-end/bloco-22-Introducao-ao-desenvolvimento-Web-com-NodeJS/dia-01-NodeJS-Um-Motor-JavaScript/exercices/imc.js
@@ -1,8 +1,34 @@
 const readline = require('readline-sync')
 
-const peso = readline.questionFloat('Qual seu peso? ')
-const altura = readline.questionInt('Qual sua altura? ');
+const peso = readline.questionFloat('Qual seu peso? (em Kg) ')
+const altura = readline.questionInt('Qual sua altura? (em cm) ');
 
-const imc = peso / altura ** altura;
+function calculaImc() {
+  const imc =(altura / Math.pow(altura / 100, 2)).toFixed(2);
+  
+  if (imc < 18.5) {
+    console.log( `${imc} Abaixo do peso (magreza)`);
+  }
 
-console.log(imc);
+  if (imc >= 18.5 && imc < 24.9 ) {
+    console.log(`Peso normal`);
+  }
+  
+  if (imc >= 25 && imc < 29.9 ) {
+    console.log(`Acima do peso (sobrepeso)`);
+  }
+
+  if (imc > 30 && imc < 34.9 ) {
+    console.log(`Obesidade grau I`);
+  }
+
+  if (imc >= 35 && imc < 39.9 ) {
+    console.log(`Obesidade grau II`);
+  }
+
+  if (imc > 40 ) {
+    console.log(`Obesidade graus III e IV`);
+  }
+}
+
+calculaImc()
