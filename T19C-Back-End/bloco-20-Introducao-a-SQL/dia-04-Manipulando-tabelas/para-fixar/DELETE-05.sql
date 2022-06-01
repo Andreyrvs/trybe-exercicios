e6081d43e5e1fe4f1d152efc8140328a9e035d2d
Andreyrvs <cortaesai@gmail.com>
2022-06-01 20:54:05 -0300

    Feat: para-fixar - Query comentada analisa as tabelas qual tipo de ON DELETE é usado no DB sakila usando 'table inspector'

---
+++ T19C-Back-End/bloco-20-Introducao-a-SQL/dia-04-Manipulando-tabelas/para-fixar/DELETE-05.sql
@@ -0,0 +1,20 @@
+/* 
+Use sakila
+
+actor - 0;
+address - ON DELETE RESTRICT;\
+category - 0;
+city - ON DELETE RESTRICT;
+country - 0;
+customer - ON DELETE RESTRICT ;
+film - ON DELETE RESTRICT ;
+film_actor -ON DELETE RESTRICT 
+film_category - ON DELETE RESTRICT;
+film_text - 0;
+inventory - ON DELETE RESTRICT;
+language - 0;
+payment - ON DELETE RESTRICT, ON DELETE SET NULL;
+rental - ON DELETE RESTRICT;
+staff - ON DELETE RESTRICT;
+store - ON DELETE RESTRICT;
+*/
