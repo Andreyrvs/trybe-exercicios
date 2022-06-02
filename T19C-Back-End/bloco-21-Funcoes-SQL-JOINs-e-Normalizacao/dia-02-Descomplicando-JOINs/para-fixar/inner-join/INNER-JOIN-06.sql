417336ac53bf8d9235a732ea1efe27366373eeb5
Andreyrvs <cortaesai@gmail.com>
2022-06-02 19:34:57 -0300

    Fix: para-fixar - 06 adiciona alias na média, muda o agrupamento dos funcionarios para 'S.staff_id'

--- T19C-Back-End/bloco-21-Funcoes-SQL-JOINs-e-Normalizacao/dia-02-Descomplicando-JOINs/para-fixar/inner-join/INNER-JOIN-06.sql
+++ T19C-Back-End/bloco-21-Funcoes-SQL-JOINs-e-Normalizacao/dia-02-Descomplicando-JOINs/para-fixar/inner-join/INNER-JOIN-06.sql
@@ -1,9 +1,11 @@
 SELECT 
-    S.first_name, S.last_name, AVG(P.amount)
+    S.first_name,
+    S.last_name,
+    AVG(P.amount) AS 'Média de pagamento'
 FROM
     sakila.staff AS S
         INNER JOIN
     sakila.payment AS P ON S.staff_id = P.staff_id
 WHERE
     YEAR(P.payment_date) = 2006
-GROUP BY S.first_name , S.last_name;
+GROUP BY S.staff_id
