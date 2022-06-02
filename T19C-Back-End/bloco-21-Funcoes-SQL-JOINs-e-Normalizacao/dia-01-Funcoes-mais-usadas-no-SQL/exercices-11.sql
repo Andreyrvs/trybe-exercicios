d7de25f455656dac3114cb49a25d3252d3376a08
Andreyrvs <cortaesai@gmail.com>
2022-06-02 01:16:59 -0300

    Feat: exercice 11 - Query troca os 3 primeiros digitos de 'phone_number' que contem '515' para '777'

---
+++ T19C-Back-End/bloco-21-Funcoes-SQL-JOINs-e-Normalizacao/dia-01-Funcoes-mais-usadas-no-SQL/exercices-11.sql
@@ -0,0 +1,10 @@
+SET SQL_SAFE_UPDATES = 0;
+
+UPDATE hr.employees 
+SET 
+    phone_number = REPLACE(phone_number, '515', '777')
+WHERE
+    phone_number LIKE '515%';
+
+
+select * from hr.employees
