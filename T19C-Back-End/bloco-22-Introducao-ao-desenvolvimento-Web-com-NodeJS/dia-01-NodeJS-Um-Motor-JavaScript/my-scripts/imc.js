4fe09be76be48f86dd461db7c97a6ec3b5f16e49
Andreyrvs <cortaesai@gmail.com>
2022-06-07 09:09:46 -0300

    Feat: exercicio 03

--- T19C-Back-End/bloco-22-Introducao-ao-desenvolvimento-Web-com-NodeJS/dia-01-NodeJS-Um-Motor-JavaScript/my-scripts/imc.js
+++ T19C-Back-End/bloco-22-Introducao-ao-desenvolvimento-Web-com-NodeJS/dia-01-NodeJS-Um-Motor-JavaScript/my-scripts/imc.js
@@ -1,5 +1,7 @@
-const PESO_PADRAO_EM_KG = 150
-const ALTURA_PADRAO_EM_METROS = 2
+const readlineSync = require('readline-sync');
+
+const PESO_PADRAO_EM_KG = readlineSync.question("Qual seu peso? (Kg) ")
+const ALTURA_PADRAO_EM_METROS = readlineSync.question("Qual sua altura? (m) ")
 
 function main() {
   const imc = PESO_PADRAO_EM_KG / ALTURA_PADRAO_EM_METROS ** 2;
