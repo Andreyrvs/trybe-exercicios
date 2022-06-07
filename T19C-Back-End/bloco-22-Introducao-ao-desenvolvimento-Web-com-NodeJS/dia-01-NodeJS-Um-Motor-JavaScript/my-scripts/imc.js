bad4b115e303c72d324c990c3452cf16c804b6f4
Andreyrvs <cortaesai@gmail.com>
2022-06-07 09:28:53 -0300

    Feat: exercice 05

--- T19C-Back-End/bloco-22-Introducao-ao-desenvolvimento-Web-com-NodeJS/dia-01-NodeJS-Um-Motor-JavaScript/my-scripts/imc.js
+++ T19C-Back-End/bloco-22-Introducao-ao-desenvolvimento-Web-com-NodeJS/dia-01-NodeJS-Um-Motor-JavaScript/my-scripts/imc.js
@@ -10,9 +10,38 @@ function main() {
   const metrosEmCm = ALTURA_PADRAO_EM_CM / METROS_PARA_CM;
   const alturaAoQuadrado = metrosEmCm ** AO_QUADRADO;
 
-  const imc = PESO_PADRAO_EM_KG / alturaAoQuadrado
-
   console.log(`Seu imc é: ${imc}`);
+  const imc = PESO_PADRAO_EM_KG / alturaAoQuadrado
+  tablelaImc(imc)
+
 }
 
-main()
+function tablelaImc(imc){
+if (imc < 18.5) {
+  console.log('Situação: Abaixo do peso (magreza)');
+  return;
+}
+
+if (imc >= 18.5 && imc < 25) {
+  console.log('Situação: Peso normal');
+  return;
+}
+
+if (imc >= 25 && imc < 30) {
+  console.log('Situação: Acima do peso (sobrepeso)');
+  return;
+}
+
+if (imc >= 30 && imc < 35) {
+  console.log('Situação: Obesidade grau I');
+  return;
+}
+
+if (imc >= 35 && imc < 40) {
+  console.log('Situação: Obesidade grau II');
+  return;
+}
+
+console.log('Situação: Obesidade graus III e IV');
+// ...
+}
