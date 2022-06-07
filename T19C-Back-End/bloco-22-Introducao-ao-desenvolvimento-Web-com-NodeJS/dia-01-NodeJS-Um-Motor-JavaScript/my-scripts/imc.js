2761cac7954679366e47fba3ab5352e614816d99
Andreyrvs <cortaesai@gmail.com>
2022-06-07 09:00:45 -0300

    Feat: exercicio 01

---
+++ T19C-Back-End/bloco-22-Introducao-ao-desenvolvimento-Web-com-NodeJS/dia-01-NodeJS-Um-Motor-JavaScript/my-scripts/imc.js
@@ -0,0 +1,9 @@
+const PESO_PADRAO_EM_KG = 150
+const ALTURA_PADRAO_EM_METROS = 2
+
+function main() {
+  const imc = PESO_PADRAO_EM_KG / ALTURA_PADRAO_EM_METROS ** 2;
+ console.log(imc);
+}
+
+main()
