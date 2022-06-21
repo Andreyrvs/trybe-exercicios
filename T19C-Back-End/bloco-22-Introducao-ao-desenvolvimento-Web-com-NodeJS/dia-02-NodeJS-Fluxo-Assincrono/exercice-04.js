e0cf91890a014c500144659cb77810b7a0790b97
Andrey Rannerson Visniewski <cortaesai@gmail.com>
2022-06-21 10:56:14 -0300

    Chore: Exercicio 04

--- T19C-Back-End/bloco-22-Introducao-ao-desenvolvimento-Web-com-NodeJS/dia-02-NodeJS-Fluxo-Assincrono/exercice-04.js
+++ T19C-Back-End/bloco-22-Introducao-ao-desenvolvimento-Web-com-NodeJS/dia-02-NodeJS-Fluxo-Assincrono/exercice-04.js
@@ -1,9 +1,7 @@
-const { json } = require('stream/consumers');
-
 const fs = require('fs').promises;
 
 function readAll() {
-  fs.readFile('./simpsons.json', 'utf-8')
+  fs.readFile('./simpsonFamily.json', 'utf-8')
     .then((fileContent) => {
       const simpsons = JSON.parse(fileContent)
       const strings = simpsons.map(({ id, name }) => `${id} - ${name}`)
@@ -31,7 +29,8 @@ function main() {
 function main() {
   // const simpson = await getSimpsonById(1)
   // console.log(simpson);
-  filterSimpsons()
+  // filterSimpsons()
+  createSimpsonFamily()
   readAll()
 }
 
@@ -52,11 +51,22 @@ async function getSimpsonById(id) {
 
 async function filterSimpsons() {
   const fileContent = await fs
-  .readFile('./simpsons.json', 'utf-8');
+    .readFile('./simpsons.json', 'utf-8');
 
   const simpsons = JSON.parse(fileContent);
 
   const newArray = simpsons.filter((simpson) => simpson.id !== '10' && simpson.id !== '6')
 
   await fs.writeFile('./simpsons.json', JSON.stringify(newArray))
+}
+
+async function createSimpsonFamily() {
+  const fileContent = await fs.readFile('./simpsons.json', 'utf-8')
+  const simpsons = JSON.parse(fileContent)
+
+  const familyIds = [1, 2, 3, 4];
+
+  const simpsonsFamily = simpsons.filter(
+    (simpson) => familyIds.includes(Number(simpson.id)));
+  await fs.writeFile('./simpsonFamily.json', JSON.stringify(simpsonsFamily))
 }
