76fa9b6207aa34963e91c0940c8ad0afec8101bc
Andrey Rannerson Visniewski <cortaesai@gmail.com>
2022-06-21 10:44:28 -0300

    Chore: Exercicio 04

--- T19C-Back-End/bloco-22-Introducao-ao-desenvolvimento-Web-com-NodeJS/dia-02-NodeJS-Fluxo-Assincrono/exercice-04.js
+++ T19C-Back-End/bloco-22-Introducao-ao-desenvolvimento-Web-com-NodeJS/dia-02-NodeJS-Fluxo-Assincrono/exercice-04.js
@@ -12,27 +12,6 @@ function readAll() {
     })
 }
 
-async function main() {
-  const simpson = await getSimpsonById(1)
-  console.log(simpson);
-  // readAll()
-}
-
-main()
-
-async function getSimpsonById(id) {
-  const fileContent = await fs.readFile('./simpsons.json', 'utf-8')
-  
-  const simpsons = JSON.parse(fileContent)
-
-  const chosenSimpson = simpsons.find((simpson) => Number(simpson.id) === id)
-
-  if (!chosenSimpson) {
-    throw new Error('id não encontrado')
-  }
-  return chosenSimpson
-}
-
 
 /* Extra: com `async/await` a função `readAll` ficaria assim:
 async function readAllComAsyncAwait() {
@@ -48,3 +27,36 @@ function main() {
 }
 ...
 */
+
+function main() {
+  // const simpson = await getSimpsonById(1)
+  // console.log(simpson);
+  filterSimpsons()
+  readAll()
+}
+
+main()
+
+async function getSimpsonById(id) {
+  const fileContent = await fs.readFile('./simpsons.json', 'utf-8')
+
+  const simpsons = JSON.parse(fileContent)
+
+  const chosenSimpson = simpsons.find((simpson) => Number(simpson.id) === id)
+
+  if (!chosenSimpson) {
+    throw new Error('id não encontrado')
+  }
+  return chosenSimpson
+}
+
+async function filterSimpsons() {
+  const fileContent = await fs
+  .readFile('./simpsons.json', 'utf-8');
+
+  const simpsons = JSON.parse(fileContent);
+
+  const newArray = simpsons.filter((simpson) => simpson.id !== '10' && simpson.id !== '6')
+
+  await fs.writeFile('./simpsons.json', JSON.stringify(newArray))
+}
