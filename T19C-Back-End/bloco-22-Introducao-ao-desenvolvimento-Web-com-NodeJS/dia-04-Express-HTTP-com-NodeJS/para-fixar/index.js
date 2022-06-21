ef2c624a6c6576b8bab8db3a37a8a75f299aa026
Andrey Rannerson Visniewski <cortaesai@gmail.com>
2022-06-21 19:23:55 -0300

    Chore: para-fixar - Query String

--- T19C-Back-End/bloco-22-Introducao-ao-desenvolvimento-Web-com-NodeJS/dia-04-Express-HTTP-com-NodeJS/para-fixar/index.js
+++ T19C-Back-End/bloco-22-Introducao-ao-desenvolvimento-Web-com-NodeJS/dia-04-Express-HTTP-com-NodeJS/para-fixar/index.js
@@ -8,12 +8,12 @@ const recipes = [
 ];
 
 const drinks = [
-	{ id: 1, name: 'Refrigerante Lata', price: 5.0 },
-	{ id: 2, name: 'Refrigerante 600ml', price: 8.0 },
-	{ id: 3, name: 'Suco 300ml', price: 4.0 },
-	{ id: 4, name: 'Suco 1l', price: 10.0 },
-	{ id: 5, name: 'Cerveja Lata', price: 4.5 },
-	{ id: 6, name: 'Água Mineral 500 ml', price: 5.0 },
+  { id: 1, name: 'Refrigerante Lata', price: 5.0 },
+  { id: 2, name: 'Refrigerante 600ml', price: 8.0 },
+  { id: 3, name: 'Suco 300ml', price: 4.0 },
+  { id: 4, name: 'Suco 1l', price: 10.0 },
+  { id: 5, name: 'Cerveja Lata', price: 4.5 },
+  { id: 6, name: 'Água Mineral 500 ml', price: 5.0 },
 ];
 
 app.get('/recipes', function (_req, res) {
@@ -24,21 +24,36 @@ app.get('/drinks', function (_req, res) {
   res.json(drinks);
 });
 
+app.get('/recipes/search', function (req, res) {
+  const { name, maxPrice, minPrice } = req.query;
+  const filteredRecipes = recipes.filter((item) => item.name.includes(name) && item.price < Number(maxPrice));
+  res.status(200).json(filteredRecipes);
+});
+
+app.get('/drinks/search', function (req, res) {
+  const {name} = req.query;
+  const filteredDrinks = drinks.filter((item) => item.name.includes(name));
+  res.status(200).json(filteredDrinks);
+})
+
+
 app.get('/recipes/:id', function (req, res) {
-  const {id} = req.params;
+  const { id } = req.params;
   const recipe = recipes.find((item) => item.id === Number(id));
 
-  if (!recipe) return res.status(404).json({ message: 'Recipe not found'});
+  if (!recipe) return res.status(404).json({ message: 'Recipe not found' });
   res.status(200).json(recipe)
 })
 
 app.get('/drinks/:id', function (req, res) {
-  const {id} = req.params;
+  const { id } = req.params;
   const drink = drinks.find((item) => item.id === Number(id))
 
-  if (!drink) return res.status(404).json({message: "Drink not found"})
+  if (!drink) return res.status(404).json({ message: "Drink not found" })
   res.status(200).json(drink)
 })
+
+
 
 app.listen(3001, () => {
   console.log('Aplicação ouvindo na porta 3001');
