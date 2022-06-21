69ac1ae0480f6c2f9a718cb725bc5cac586cbddd
Andrey Rannerson Visniewski <cortaesai@gmail.com>
2022-06-21 17:50:57 -0300

    Chore: para-fixar - Parâmetros de rota

--- T19C-Back-End/bloco-22-Introducao-ao-desenvolvimento-Web-com-NodeJS/dia-04-Express-HTTP-com-NodeJS/para-fixar/index.js
+++ T19C-Back-End/bloco-22-Introducao-ao-desenvolvimento-Web-com-NodeJS/dia-04-Express-HTTP-com-NodeJS/para-fixar/index.js
@@ -16,13 +16,29 @@ const drinks = [
 	{ id: 6, name: 'Água Mineral 500 ml', price: 5.0 },
 ];
 
-app.get('/recipes', function (req, res) {
+app.get('/recipes', function (_req, res) {
   res.json(recipes)
 });
 
-app.get('/drinks', function (req, res) {
+app.get('/drinks', function (_req, res) {
   res.json(drinks);
 });
+
+app.get('/recipes/:id', function (req, res) {
+  const {id} = req.params;
+  const recipe = recipes.find((item) => item.id === Number(id));
+
+  if (!recipe) return res.status(404).json({ message: 'Recipe not found'});
+  res.status(200).json(recipe)
+})
+
+app.get('/drinks/:id', function (req, res) {
+  const {id} = req.params;
+  const drink = drinks.find((item) => item.id === Number(id))
+
+  if (!drink) return res.status(404).json({message: "Drink not found"})
+  res.status(200).json(drink)
+})
 
 app.listen(3001, () => {
   console.log('Aplicação ouvindo na porta 3001');
