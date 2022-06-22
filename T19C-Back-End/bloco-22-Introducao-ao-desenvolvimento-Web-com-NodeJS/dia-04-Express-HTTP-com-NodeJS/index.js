3573ae1b74e7c89f82f2338cd362632308aff435
Andrey Rannerson Visniewski <cortaesai@gmail.com>
2022-06-22 15:19:54 -0300

    Chore: exercicio 03

--- T19C-Back-End/bloco-22-Introducao-ao-desenvolvimento-Web-com-NodeJS/dia-04-Express-HTTP-com-NodeJS/index.js
+++ T19C-Back-End/bloco-22-Introducao-ao-desenvolvimento-Web-com-NodeJS/dia-04-Express-HTTP-com-NodeJS/index.js
@@ -11,7 +11,15 @@ app.get('/ping', function (_req, res) {
 
 app.post('/hello', function (req, res) {
   const { name } = req.body;
-  return res.status(200).json({ message: `Hello, ${name}!` })
+  return res.status(200).json({ "message": `Hello, ${name}!` })
+})
+
+app.post('/greetings', function (req, res) {
+  const {name, age} = req.body;
+  if (Number(age) > 17) {
+    return res.status(200).json({"message": `Hello, ${name}!`})
+  }
+  return res.status(401).json({"message": "Unauthorized"})
 })
 
 app.listen(applicationPort, () => {
