8c08d8488a6f847880da1e42bb88845b0a4bf694
Andrey Rannerson Visniewski <cortaesai@gmail.com>
2022-06-22 15:11:25 -0300

    Chore: exercicio 01

--- T19C-Back-End/bloco-22-Introducao-ao-desenvolvimento-Web-com-NodeJS/dia-04-Express-HTTP-com-NodeJS/index.js
+++ T19C-Back-End/bloco-22-Introducao-ao-desenvolvimento-Web-com-NodeJS/dia-04-Express-HTTP-com-NodeJS/index.js
@@ -5,16 +5,13 @@ app.use(bodyParser.json())
 
 const applicationPort = 3031
 
-const userName = []
-
 app.get('/ping', function (_req, res) {
-  res.status(200).json({message: "pong"})
+  res.status(200).json({ message: "pong" })
 })
 
 app.post('/hello', function (req, res) {
-  const {name} = req.body;
-  userName.push({name});
-  res.status(201).json({message: `Hello, ${name}`})
+  const { name } = req.body;
+  return res.status(200).json({ message: `Hello, ${name}!` })
 })
 
 app.listen(applicationPort, () => {
