481f5adbb4185ff40d13ad12310920c18d926e27
Andrey Rannerson Visniewski <cortaesai@gmail.com>
2022-06-22 15:07:52 -0300

    Fix: exercicio 01

--- T19C-Back-End/bloco-22-Introducao-ao-desenvolvimento-Web-com-NodeJS/dia-04-Express-HTTP-com-NodeJS/index.js
+++ T19C-Back-End/bloco-22-Introducao-ao-desenvolvimento-Web-com-NodeJS/dia-04-Express-HTTP-com-NodeJS/index.js
@@ -1,12 +1,21 @@
+const bodyParser = require('body-parser');
 const express = require('express');
 const app = express();
+app.use(bodyParser.json())
+
 const applicationPort = 3031
 
-app.get('/ping', function (req, res) {
-  res.status(200).send({message: "pong"})
+const userName = []
+
+app.get('/ping', function (_req, res) {
+  res.status(200).json({message: "pong"})
 })
 
-
+app.post('/hello', function (req, res) {
+  const {name} = req.body;
+  userName.push({name});
+  res.status(201).json({message: `Hello, ${name}`})
+})
 
 app.listen(applicationPort, () => {
   console.log(`Aplicação escutando na porta ${applicationPort}`);
