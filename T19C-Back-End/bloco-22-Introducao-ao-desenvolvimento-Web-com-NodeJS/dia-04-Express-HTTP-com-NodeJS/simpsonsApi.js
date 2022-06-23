104a2cd1655d91b7e1feda4ece42df7295379351
Andrey Rannerson Visniewski <cortaesai@gmail.com>
2022-06-23 09:28:05 -0300

    Chore: exercicio 07

--- T19C-Back-End/bloco-22-Introducao-ao-desenvolvimento-Web-com-NodeJS/dia-04-Express-HTTP-com-NodeJS/simpsonsApi.js
+++ T19C-Back-End/bloco-22-Introducao-ao-desenvolvimento-Web-com-NodeJS/dia-04-Express-HTTP-com-NodeJS/simpsonsApi.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 
-const {getSimpsons, setSimpsons} = require('./simpsons.js')
+const { getSimpsons, setSimpsons } = require('./simpsons.js')
 
 const app = express()
 app.use(bodyParser.json());
@@ -11,13 +11,28 @@ const port = 3031
 app.get('/simpsons', async (_req, res) => {
   try {
     const simpsons = await getSimpsons();
-    
+
     res.status(200).json(simpsons)
   } catch (error) {
     return res.status(500).end()
   }
 })
 
+app.get('/simpsons/:id', async (req, res) => {
+  try {
+    const { id: paramsID } = req.params
+    const simpsonsUtils = await getSimpsons()
+    const findSimpson = simpsonsUtils.find(
+      ({ id }) => id === paramsID)
 
+    if (!findSimpson) {
+      return res.status(404).json({ message: 'simpson not found' })
+    }
+    return res.status(200).json(findSimpson)
+
+  } catch (error) {
+    return res.status(500).end();
+  }
+})
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
