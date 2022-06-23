e8d46b734c9deb32c445d05c60839ab55f2636c2
Andrey Rannerson Visniewski <cortaesai@gmail.com>
2022-06-23 12:55:25 -0300

    Chore: para-fixar - Visualizando o conteúdo das requisições no Console

--- T19C-Back-End/bloco-22-Introducao-ao-desenvolvimento-Web-com-NodeJS/dia-05-Express-Middlewares/para-fixar/server.js
+++ T19C-Back-End/bloco-22-Introducao-ao-desenvolvimento-Web-com-NodeJS/dia-05-Express-Middlewares/para-fixar/server.js
@@ -5,7 +5,15 @@ const app = express();
 
 app.use(bodyParser.json());
 app.use(cors())
-
+app.use((req, _res, next) => {
+  console.log('req.method:', req.method);
+  console.log('req.path:', req.path);
+  console.log('req.params:', req.params);
+  console.log('req.query:', req.query);
+  console.log('req.headers:', req.headers);
+  console.log('req.body:', req.body);
+  next();
+});
 app.post('/hello', (req, res) => {
   // req.body agora está disponível
   res.status(200).json({ greeting: `Hello, ${req.body.name}!` });
