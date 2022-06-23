abb2be84e0d48b36db616f123f4c80d34270f951
Andrey Rannerson Visniewski <cortaesai@gmail.com>
2022-06-23 20:39:36 -0300

    Feat: Diretorio de middleware com validacão para 'productName'

--- T19C-Back-End/bloco-22-Introducao-ao-desenvolvimento-Web-com-NodeJS/dia-05-Express-Middlewares/index.js
+++ T19C-Back-End/bloco-22-Introducao-ao-desenvolvimento-Web-com-NodeJS/dia-05-Express-Middlewares/index.js
@@ -1,19 +1,13 @@
 const express = require('express')
 const bodyParser = require('body-parser');
+const validateProducName = require('./middlewares/validateProducName')
 
 const PORT = 3001
 const app = express();
 app.use(bodyParser.json());
 
-app.post('/sales', (req, res) => {
-  const { productName, infos } = req.body;
-  if (!productName) {
-    return res.status(400).json({ "message": "O campo producName é obrigatório" })
-  }
-  if (productName.length < 4) {
-    return res.status(400).json({ "message": "O campo productName deve ter pelo menos 4 caracteres" })
-  }
-
+app.post('/sales', validateProducName, (req, res) => {
+  res.status(201).json({ message: 'Sale created successfully!' })
 })
 
 app.listen(PORT, () => console.log(`Aplicação escutando na porta ${PORT}`))
