907f573cb557b5d5d7f4d9580ef8c21cfafe5de8
Andrey Rannerson Visniewski <cortaesai@gmail.com>
2022-06-23 21:26:51 -0300

    Chore: adiciona gerador de token e valida a rota signup

--- T19C-Back-End/bloco-22-Introducao-ao-desenvolvimento-Web-com-NodeJS/dia-05-Express-Middlewares/index.js
+++ T19C-Back-End/bloco-22-Introducao-ao-desenvolvimento-Web-com-NodeJS/dia-05-Express-Middlewares/index.js
@@ -4,15 +4,25 @@ const validateProducName = require('./middlewares/validateProducName')
 const validateInfo = require('./middlewares/validateInfo')
 const validateSaleDate = require('./middlewares/validateSaleDate')
 const validateWarrantyPeriod = require('./middlewares/validateWarrantyPeriod')
+const validateSignup = require('./middlewares/validateSignup')
+const crypto = require('crypto');
 
 const PORT = 3001
 const app = express();
 app.use(bodyParser.json());
 
+function generateToken() {
+  return crypto.randomBytes(8).toString('hex');
+}
+
 app.post('/sales',
   validateProducName, validateInfo, validateSaleDate, validateWarrantyPeriod,
-  (req, res) => {
+  (_req, res) => {
     res.status(201).json({ "message": "Venda cadastrada com sucesso" })
   })
+
+app.post('/signup',validateSignup,(_req, res) => {
+  return res.status(201).json({"token": `${generateToken()}`})
+})
 
 app.listen(PORT, () => console.log(`Aplicação escutando na porta ${PORT}`))
