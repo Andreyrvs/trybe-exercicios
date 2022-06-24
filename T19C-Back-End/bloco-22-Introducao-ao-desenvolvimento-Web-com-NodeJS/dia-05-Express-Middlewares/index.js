61aea74b5aa2099300442d36875a14c665980c0d
Andrey Rannerson Visniewski <cortaesai@gmail.com>
2022-06-23 21:17:48 -0300

    Chore: cria middleware para validar o campo 'warrantyPeriod'

--- T19C-Back-End/bloco-22-Introducao-ao-desenvolvimento-Web-com-NodeJS/dia-05-Express-Middlewares/index.js
+++ T19C-Back-End/bloco-22-Introducao-ao-desenvolvimento-Web-com-NodeJS/dia-05-Express-Middlewares/index.js
@@ -2,14 +2,17 @@ const express = require('express')
 const bodyParser = require('body-parser');
 const validateProducName = require('./middlewares/validateProducName')
 const validateInfo = require('./middlewares/validateInfo')
-const validateSaleDate= require('./middlewares/validateSaleDate')
+const validateSaleDate = require('./middlewares/validateSaleDate')
+const validateWarrantyPeriod = require('./middlewares/validateWarrantyPeriod')
 
 const PORT = 3001
 const app = express();
 app.use(bodyParser.json());
 
-app.post('/sales', validateProducName, validateInfo, validateSaleDate, (req, res) => {
-  res.status(201).json({ message: 'Sale created successfully!' })
-})
+app.post('/sales',
+  validateProducName, validateInfo, validateSaleDate, validateWarrantyPeriod,
+  (req, res) => {
+    res.status(201).json({ "message": "Venda cadastrada com sucesso" })
+  })
 
 app.listen(PORT, () => console.log(`Aplicação escutando na porta ${PORT}`))
