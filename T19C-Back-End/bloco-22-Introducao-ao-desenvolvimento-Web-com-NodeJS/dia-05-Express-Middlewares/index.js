2ce80782d9e037330c034d6c3d2476a354bcc7b0
Andrey Rannerson Visniewski <cortaesai@gmail.com>
2022-06-23 21:06:54 -0300

    Chore: cria middleware para validar o campo 'saleDate'

--- T19C-Back-End/bloco-22-Introducao-ao-desenvolvimento-Web-com-NodeJS/dia-05-Express-Middlewares/index.js
+++ T19C-Back-End/bloco-22-Introducao-ao-desenvolvimento-Web-com-NodeJS/dia-05-Express-Middlewares/index.js
@@ -2,12 +2,13 @@ const express = require('express')
 const bodyParser = require('body-parser');
 const validateProducName = require('./middlewares/validateProducName')
 const validateInfo = require('./middlewares/validateInfo')
+const validateSaleDate= require('./middlewares/validateSaleDate')
 
 const PORT = 3001
 const app = express();
 app.use(bodyParser.json());
 
-app.post('/sales', validateProducName, validateInfo, (req, res) => {
+app.post('/sales', validateProducName, validateInfo, validateSaleDate, (req, res) => {
   res.status(201).json({ message: 'Sale created successfully!' })
 })
 
