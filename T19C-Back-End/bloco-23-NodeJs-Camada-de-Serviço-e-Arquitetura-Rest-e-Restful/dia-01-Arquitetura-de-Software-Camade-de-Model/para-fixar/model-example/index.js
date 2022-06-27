e15d5e7bdb1582244d2a52adc442b8ce9adc444e
Andrey Rannerson Visniewski <cortaesai@gmail.com>
2022-06-26 21:00:07 -0300

    Chore: Cria rota '/books' com verbo http 'GET' para trazer todos os livro

--- T19C-Back-End/bloco-23-NodeJs-Camada-de-Serviço-e-Arquitetura-Rest-e-Restful/dia-01-Arquitetura-de-Software-Camade-de-Model/para-fixar/model-example/index.js
+++ T19C-Back-End/bloco-23-NodeJs-Camada-de-Serviço-e-Arquitetura-Rest-e-Restful/dia-01-Arquitetura-de-Software-Camade-de-Model/para-fixar/model-example/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 
 const Author = require('./models/Author');
+const Books = require('./models/Books');
 
 const app = express();
 
@@ -8,6 +9,12 @@ app.get('/authors', async (_req, res) => {
   const authors = await Author.getAll();
 
   res.status(200).json(authors);
+});
+
+app.get('/books', async (_req, res) => {
+  const books = await Books.getAll();
+
+  res.status(200).json(books);
 });
 
 const PORT = process.env.PORT || 3001; 
