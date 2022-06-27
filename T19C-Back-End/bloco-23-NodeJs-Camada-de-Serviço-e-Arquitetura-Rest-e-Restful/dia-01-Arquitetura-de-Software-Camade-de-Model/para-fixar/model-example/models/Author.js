27e199d42558fae5a17a70feed0132cd16bddab4
Andrey Rannerson Visniewski <cortaesai@gmail.com>
2022-06-26 22:03:33 -0300

    Chore: cria método para obter detalhes de uma pessoa escritora

--- T19C-Back-End/bloco-23-NodeJs-Camada-de-Serviço-e-Arquitetura-Rest-e-Restful/dia-01-Arquitetura-de-Software-Camade-de-Model/para-fixar/model-example/models/Author.js
+++ T19C-Back-End/bloco-23-NodeJs-Camada-de-Serviço-e-Arquitetura-Rest-e-Restful/dia-01-Arquitetura-de-Software-Camade-de-Model/para-fixar/model-example/models/Author.js
@@ -2,7 +2,7 @@ const connection = require('./connection');
 
 const getNewAuthor = ({ id, firstName, middleName, lastName }) => {
   const fullName = [firstName, middleName, lastName]
-  .filter(Boolean)
+    .filter(Boolean)
     .join(' ');
 
   return {
@@ -28,6 +28,25 @@ const getAll = async () => {
   return authors.map(serialize).map(getNewAuthor);
 };
 
+const findById = async (id) => {
+  const query = `SELECT id, first_name, middle_name, last_name 
+  FROM model_example.authors
+  WHERE id = ? `;
+  const [authorData] = await connection.execute(query, [id]);
+
+  if (authorData.length === 0) return null;
+
+  const { firstName, middleName, lastName } = authorData.map(serialize);
+
+  return getNewAuthor({
+    id,
+    firstName,
+    middleName,
+    lastName,
+  });
+};
+
 module.exports = {
   getAll,
+  findById,
 };
