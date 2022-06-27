8e95cf468fa892926f2c2d741bcd3f3c48475f4b
Andrey Rannerson Visniewski <cortaesai@gmail.com>
2022-06-26 23:16:25 -0300

    Chore: cria modelo que valida e adicona pessoas escritoras no bando de dados

--- T19C-Back-End/bloco-23-NodeJs-Camada-de-Serviço-e-Arquitetura-Rest-e-Restful/dia-01-Arquitetura-de-Software-Camade-de-Model/para-fixar/model-example/index.js
+++ T19C-Back-End/bloco-23-NodeJs-Camada-de-Serviço-e-Arquitetura-Rest-e-Restful/dia-01-Arquitetura-de-Software-Camade-de-Model/para-fixar/model-example/index.js
@@ -56,6 +56,18 @@ app.post('/authors', async (req, res) => {
   res.status(201).json({ message: 'Autor criado com sucesso!' });
 });
 
+app.post('/books', async (req, res) => {
+  const { title, author_id } = req.body;
+
+  if (!Books.isValid(title, author_id)) {
+    return res.status(400).json({ message: 'Dados inválidos' });
+  }
+
+  await Books.create(title, author_id);
+
+  res.status(201).json({ message: 'Livro criado com sucesso!' });
+});
+
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
--- T19C-Back-End/bloco-23-NodeJs-Camada-de-Serviço-e-Arquitetura-Rest-e-Restful/dia-01-Arquitetura-de-Software-Camade-de-Model/para-fixar/model-example/models/Books.js
+++ T19C-Back-End/bloco-23-NodeJs-Camada-de-Serviço-e-Arquitetura-Rest-e-Restful/dia-01-Arquitetura-de-Software-Camade-de-Model/para-fixar/model-example/models/Books.js
@@ -43,9 +43,8 @@ const isValid = async (title, authorId) => {
 };
 
 const create = async (title, authorId) => connection.execute(
-  `INSERT INTO model_example.books
-  (title, author_id)
-  VAlUES (?, ?)`, [title, authorId],
+  'INSERT INTO model_example.books (title, author_id) VAlUES (?, ?)',
+  [title, authorId],
 );
 
 module.exports = {
