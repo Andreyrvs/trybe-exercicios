61bd9fe4e7f1a15163977294072bc7a5c6822ada
Andrey Rannerson Visniewski <cortaesai@gmail.com>
2022-06-28 13:07:15 -0300

    Chore: cria modelo que adiciona usuarios e valida os dados

--- T19C-Back-End/bloco-23-NodeJs-Camada-de-Servico-e-Arquitetura-Rest-e-Restful/dia-01-Arquitetura-de-Software-Camade-de-Model/index.js
+++ T19C-Back-End/bloco-23-NodeJs-Camada-de-Servico-e-Arquitetura-Rest-e-Restful/dia-01-Arquitetura-de-Software-Camade-de-Model/index.js
@@ -3,7 +3,8 @@ const express = require('express');
 
 const app = express();
 app.use(bodyParser.json());
-const port = 3000;
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.post('/user', (req, res) => res.send('Hello World!'));
+
+const port = 3000;
 app.listen(port, () => console.log(`Aplicação ouvino na porta: ${port}!`));
--- T19C-Back-End/bloco-23-NodeJs-Camada-de-Servico-e-Arquitetura-Rest-e-Restful/dia-01-Arquitetura-de-Software-Camade-de-Model/models/User.js
+++ T19C-Back-End/bloco-23-NodeJs-Camada-de-Servico-e-Arquitetura-Rest-e-Restful/dia-01-Arquitetura-de-Software-Camade-de-Model/models/User.js
@@ -1,1 +1,24 @@
 const connection = require('./connection');
+
+// const serialize = (userData) => ({
+//   id: userData.id,
+//   firstName: userData.first_name,
+//   lastName: userData.last_name,
+// });
+
+const isValid = async (firstName, lastName, email, password) => {
+  if (!firstName || !lastName || !email || !password) return null;
+  if (typeof password === 'string' || password < 6) return null;
+};
+
+const create = async (firstName, lastName, email, password) => connection.execute(
+  `INSERT INTO camada_de_model.user
+  (first_name, last_name, email, password)
+  VALUES
+  (?, ?, ?, ?)`, [firstName, lastName, email, password],
+);
+
+module.exports = {
+  create,
+  isValid,
+};
