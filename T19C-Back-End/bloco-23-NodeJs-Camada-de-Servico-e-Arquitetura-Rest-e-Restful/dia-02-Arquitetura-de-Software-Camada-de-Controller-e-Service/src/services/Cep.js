fd80e34f2093a0622ffd5c93cb9aa8b9f4b35405
Andrey Rannerson Visniewski <cortaesai@gmail.com>
2022-06-30 19:39:43 -0300

    Fix: (Services) - Formata corretamente o cep

--- T19C-Back-End/bloco-23-NodeJs-Camada-de-Servico-e-Arquitetura-Rest-e-Restful/dia-02-Arquitetura-de-Software-Camada-de-Controller-e-Service/src/services/Cep.js
+++ T19C-Back-End/bloco-23-NodeJs-Camada-de-Servico-e-Arquitetura-Rest-e-Restful/dia-02-Arquitetura-de-Software-Camada-de-Controller-e-Service/src/services/Cep.js
@@ -1,7 +1,27 @@
 const Cep = require('../models/Cep');
 
 const REGEX_CEP = /^\d{5}-\d{3}$/gm;
-// /^[0-9]{5}-[0-9]{3}$/gm;
+
+const formatCep = (cep) => {
+  // Caso o CEP já esteja formatado, retorna o próprio CEP
+  if (REGEX_CEP.test(cep)) return cep;
+
+  // Caso não esteja formatado, utiliza regex para adicionar a formatação
+  return cep.replace(/(\d{5})(\d{3})/, '$1-$2');
+};
+
+// Formata os campos para exibição
+const getNewCep = (cep, result) => {
+  // Desestrutua o array e depois o objeto onde estão as informaçoes
+  const [{ logradouro, bairro, localidade, uf }] = result;
+  return {
+    cep: formatCep(cep),
+    logradouro,
+    bairro,
+    localidade,
+    uf,
+  };
+};
 
 const getById = async (cep) => {
   if (!REGEX_CEP.test(cep)) {
@@ -19,8 +39,8 @@ const getById = async (cep) => {
       },
     };
   }
-
-  return result;
+  const newResult = getNewCep(cep, result);
+  return newResult;
 };
 
 module.exports = {
