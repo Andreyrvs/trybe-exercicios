1ca8c8e1bc16f7e6e9365052346048fcd653a2d6
Andrey Rannerson Visniewski <cortaesai@gmail.com>
2022-06-30 12:30:15 -0300

    Chore: config antes de refatorar

--- T19C-Back-End/bloco-23-NodeJs-Camada-de-Servico-e-Arquitetura-Rest-e-Restful/dia-02-Arquitetura-de-Software-Camada-de-Controller-e-Service/src/para-fixar/model-example/models/connection.js
+++ T19C-Back-End/bloco-23-NodeJs-Camada-de-Servico-e-Arquitetura-Rest-e-Restful/dia-02-Arquitetura-de-Software-Camada-de-Controller-e-Service/src/para-fixar/model-example/models/connection.js
@@ -1,12 +1,12 @@
 const mysql = require('mysql2/promise');
-
+require('dotenv').config();
 // No lugar de local host usar ( host: 'backend_Mysql',) é o nome do contêiner que tem o MySQL
 
 const connection = mysql.createPool({
-  host: 'backend_Mysql',
-  user: 'root',
-  password: 'root',
-  database: 'model_example',
+  host: process.env.MYSQL_HOST,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE,
 });
 
 module.exports = connection;
