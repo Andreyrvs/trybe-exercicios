ac365f00732355d11f9a4116b636fd0218fa9fba
Andrey Rannerson Visniewski <cortaesai@gmail.com>
2022-06-30 18:52:59 -0300

    Chore: Adiciona rescue e middleware de erros, Corrige nome da rota '/cep/:cep'

--- T19C-Back-End/bloco-23-NodeJs-Camada-de-Servico-e-Arquitetura-Rest-e-Restful/dia-02-Arquitetura-de-Software-Camada-de-Controller-e-Service/src/index.js
+++ T19C-Back-End/bloco-23-NodeJs-Camada-de-Servico-e-Arquitetura-Rest-e-Restful/dia-02-Arquitetura-de-Software-Camada-de-Controller-e-Service/src/index.js
@@ -1,8 +1,21 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const rescue = require('express-rescue');
+
 require('dotenv').config();
 
+const errorMiddleware = require('./middlewares/error');
+
 const app = express();
-const port = process.env.API_PORT;
+app.use(bodyParser.json());
+
+const Cep = require('./controllers/Cep');
 
 app.get('/ping', (req, res) => res.status(200).json({ message: 'pong!' }));
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.get('/cep/:cep', rescue(Cep.getById));
+
+app.use(errorMiddleware);
+
+const port = process.env.API_PORT || 3000;
+
+app.listen(port, () => console.log(`Aplicação escutando a porta: ${port}!`));
