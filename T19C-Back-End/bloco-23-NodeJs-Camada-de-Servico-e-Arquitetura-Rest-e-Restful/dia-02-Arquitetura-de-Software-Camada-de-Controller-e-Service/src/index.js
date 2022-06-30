bd08723b938b77ada3970e9bc3be9c96f863be6d
Andrey Rannerson Visniewski <cortaesai@gmail.com>
2022-06-30 15:22:04 -0300

    Chore: Exercicio 01 - Cria rota ping

--- T19C-Back-End/bloco-23-NodeJs-Camada-de-Servico-e-Arquitetura-Rest-e-Restful/dia-02-Arquitetura-de-Software-Camada-de-Controller-e-Service/src/index.js
+++ T19C-Back-End/bloco-23-NodeJs-Camada-de-Servico-e-Arquitetura-Rest-e-Restful/dia-02-Arquitetura-de-Software-Camada-de-Controller-e-Service/src/index.js
@@ -1,7 +1,8 @@
 const express = require('express');
+require('dotenv').config();
 
 const app = express();
-const port = 3000;
+const port = process.env.API_PORT;
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/ping', (req, res) => res.status(200).json({ message: 'pong!' }));
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
