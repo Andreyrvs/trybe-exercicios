e3e58d8f2cc3a985816025777cea21f924dab4f1
Andrey Rannerson Visniewski <cortaesai@gmail.com>
2022-06-30 20:55:02 -0300

    Chore: (Model) - Insere 'cep, logradouro, bairro, localidade, uf' na tabela 'ceps' do bando de dados 'cep_lookup'. Retorna os dados em um Objeto

--- T19C-Back-End/bloco-23-NodeJs-Camada-de-Servico-e-Arquitetura-Rest-e-Restful/dia-02-Arquitetura-de-Software-Camada-de-Controller-e-Service/src/models/Cep.js
+++ T19C-Back-End/bloco-23-NodeJs-Camada-de-Servico-e-Arquitetura-Rest-e-Restful/dia-02-Arquitetura-de-Software-Camada-de-Controller-e-Service/src/models/Cep.js
@@ -7,10 +7,25 @@ const getById = async (cep) => {
 
   if (rows.length === 0) return null;
 
-  console.log('model', rows);
   return rows;
+};
+
+const createCep = async ({ cep, logradouro, bairro, localidade, uf }) => {
+  const query = `INSERT INTO cep_lookup.ceps (cep, logradouro, bairro, localidade, uf)
+  VALUES (?,?,?,?,?)`;
+
+  await connection.execute(query, [cep, logradouro, bairro, localidade, uf]);
+
+  return {
+    cep,
+    logradouro,
+    bairro,
+    localidade,
+    uf,
+  };
 };
 
 module.exports = {
   getById,
+  createCep,
 };
