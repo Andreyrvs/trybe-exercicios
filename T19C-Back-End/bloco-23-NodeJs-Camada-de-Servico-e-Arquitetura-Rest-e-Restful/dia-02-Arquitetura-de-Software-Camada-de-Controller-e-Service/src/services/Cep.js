9e94f28857e6e0dcb882ae28bd24ed01a5995f73
Andrey Rannerson Visniewski <cortaesai@gmail.com>
2022-06-30 21:52:40 -0300

    chmod: 777 - nesses arquivos

--- T19C-Back-End/bloco-23-NodeJs-Camada-de-Servico-e-Arquitetura-Rest-e-Restful/dia-02-Arquitetura-de-Software-Camada-de-Controller-e-Service/src/services/Cep.js
+++ T19C-Back-End/bloco-23-NodeJs-Camada-de-Servico-e-Arquitetura-Rest-e-Restful/dia-02-Arquitetura-de-Software-Camada-de-Controller-e-Service/src/services/Cep.js
@@ -1,4 +1,5 @@
 const Cep = require('../models/Cep');
+const ViaCep = require('../models/ViaCep');
 
 const REGEX_CEP = /^\d{5}-\d{3}$/gm;
 
@@ -10,18 +11,18 @@ const formatCep = (cep) => {
   return cep.replace(/(\d{5})(\d{3})/, '$1-$2');
 };
 
-const getNewCepById = (cep, result) => {
-  // Desestrutua o array e depois o objeto onde estão as informaçoes
+// const getNewCepById = (cep, result) => {
+//   // Desestrutua o array e depois o objeto onde estão as informaçoes
 
-  const [{ logradouro, bairro, localidade, uf }] = result;
-  return {
-    cep: formatCep(cep),
-    logradouro,
-    bairro,
-    localidade,
-    uf,
-  };
-};
+//   const [{ logradouro, bairro, localidade, uf }] = result;
+//   return {
+//     cep: formatCep(cep),
+//     logradouro,
+//     bairro,
+//     localidade,
+//     uf,
+//   };
+// };
 
 // Formata os campos para exibição
 const getNewCep = (cep, result) => {
@@ -46,18 +47,49 @@ const errorMessage = (code, message) => ({
 
 const cepIsValid = (cep) => REGEX_CEP.test(cep);
 
-const getById = async (cep) => {
-  if (!cepIsValid(cep)) {
+// Antes de aplicar o Axios buscando no ViaCep
+// const getById = async (cep) => {
+//   if (!cepIsValid(cep)) {
+//     return errorMessage('invalidData', 'CEP inválido');
+//   }
+//   const treatedCep = cep.replace('-', '');
+
+//   const result = await Cep.getById(treatedCep);
+
+//   if (!result) return errorMessage('notFound', 'CEP não encontrado');
+
+//   const newResult = getNewCepById(cep, result);
+//   return newResult;
+// };
+
+/* ... */
+
+const getById = async (searchedCep) => {
+  // Começamos buscando o CEP no banco de dados
+  if (!cepIsValid(searchedCep)) {
     return errorMessage('invalidData', 'CEP inválido');
   }
-  const treatedCep = cep.replace('-', '');
+  const treatedCep = searchedCep.replace('-', '');
 
-  const result = await Cep.getById(treatedCep);
+  const cepId = await Cep.getById(treatedCep);
 
-  if (!result) return errorMessage('notFound', 'CEP não encontrado');
+  // Caso encontremos, retornamos sem consultar a API
+  if (cepId) {
+    return cepId;
+  }
 
-  const newResult = getNewCepById(cep, result);
-  return newResult;
+  // Caso o CEP não exista no banco de dados, buscamos na API
+  const cepFromApi = await ViaCep.lookupCep(searchedCep);
+
+  // Caso o CEP não exista na API,
+  // retornamos um erro dizendo que
+  // o CEP não foi encontrado
+  if (!cepFromApi) return errorMessage('notFound', 'CEP não encontrado');
+  // Caso o CEP exista na API, pedimos ao model
+  // que armazene-o no banco e retornamos
+  // o resultado
+  const { cep, logragouro, bairro, localidade, uf } = cepFromApi;
+  return Cep.createCep({ cep, logragouro, bairro, localidade, uf });
 };
 
 const createCep = async ({ cep: rawCep, logradouro, bairro, localidade, uf }) => {
