9b3b8b8fa894dfc71adfdefecdffda855d48f00f
Andrey Rannerson Visniewski <cortaesai@gmail.com>
2022-07-03 21:53:35 -0300

    Chore: Implementa a rota no index

---
+++ T19C-Back-End/bloco-23-NodeJs-Camada-de-Servico-e-Arquitetura-Rest-e-Restful/dia-04-Arquitetura-de-Software-Testando-Camadas/src/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+require('dotenv').config();
+
+const MovieController = require('./controllers/movieController');
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.post('/movies', MovieController.create);
+
+const PORT = process.env.API_PORT || 3000;
+
+app.get('/', (req, res) => res.send('Hello World!'));
+app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`));
