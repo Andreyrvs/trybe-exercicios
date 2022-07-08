69999ba3d6d3e51ca474c78312ea26b264ec97e1
Andrey Rannerson Visniewski <cortaesai@gmail.com>
2022-07-08 15:31:42 -0300

    para-fixar - Configuando o Sequilize até Seeders

---
+++ T19C-Back-End/bloco-24-NodeJs-ORM-e-Autenticacao/dia-01-ORM-Interface-da-aplicacao-com-o-banco-de-dados/para-fixar/app-with-sequelize/migrations/20220708155345-create-user.js
@@ -0,0 +1,29 @@
+/* eslint-disable max-lines-per-function */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      fullName: {
+        type: Sequelize.STRING,
+      },
+      email: {
+        type: Sequelize.STRING,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      } });
+  },
+  async down(queryInterface, __Sequelize) {
+    await queryInterface.dropTable('Users');
+  },
+};
---
+++ T19C-Back-End/bloco-24-NodeJs-ORM-e-Autenticacao/dia-01-ORM-Interface-da-aplicacao-com-o-banco-de-dados/para-fixar/app-with-sequelize/seeders/20220708182324-users.js
@@ -0,0 +1,19 @@
+module.exports = {
+  up: async (queryInterface, Sequelize) => queryInterface.bulkInsert('Users',
+  [
+    {
+      fullName: 'Leonardo',
+      email: 'leo@test.com',
+      createdAt: Sequelize.literal('CURRENT_TIMESTAMP'),
+      updatedAt: Sequelize.literal('CURRENT_TIMESTAMP'),
+    },
+    {
+      fullName: 'JEduardo',
+      email: 'edu@test.com',
+      createdAt: Sequelize.literal('CURRENT_TIMESTAMP'),
+      updatedAt: Sequelize.literal('CURRENT_TIMESTAMP'),
+    },
+  ], {}),
+
+  down: async (queryInterface, Sequelize) => queryInterface.bulkDelete('Users', null, {}),
+};
