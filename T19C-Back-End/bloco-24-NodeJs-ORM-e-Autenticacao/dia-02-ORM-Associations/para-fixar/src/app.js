34b47c4442018c863fbda94421b34ea66d3575be
Andrey Rannerson Visniewski <cortaesai@gmail.com>
2022-07-13 20:51:11 -0300

    chore: (para-fixar) - Cria rotas

--- T19C-Back-End/bloco-24-NodeJs-ORM-e-Autenticacao/dia-02-ORM-Associations/para-fixar/src/app.js
+++ T19C-Back-End/bloco-24-NodeJs-ORM-e-Autenticacao/dia-02-ORM-Associations/para-fixar/src/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 
 const EmployeeController = require('./controllers/EmployeeController');
+const UserBookController = require('./controllers/UserBookController');
 
 const app = express();
 
@@ -9,5 +10,7 @@ app.use(express.json());
 app.get('/employees', EmployeeController.getAll);
 app.get('/eagerLoading/:id', EmployeeController.getByIdEager);
 app.get('/lazyLoading/:id', EmployeeController.getByIdLazy);
+
+app.get('/usersbooks/:id', UserBookController.getById);
 
 module.exports = app;
