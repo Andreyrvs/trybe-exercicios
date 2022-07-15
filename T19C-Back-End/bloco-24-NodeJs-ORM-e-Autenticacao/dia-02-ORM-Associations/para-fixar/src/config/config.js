7503b30990de0ae4525dc22b0cd911683ffe6d2a
Andrey Rannerson Visniewski <cortaesai@gmail.com>
2022-07-14 22:15:00 -0300

    adiciona como usaar a config de test

--- T19C-Back-End/bloco-24-NodeJs-ORM-e-Autenticacao/dia-02-ORM-Associations/para-fixar/src/config/config.js
+++ T19C-Back-End/bloco-24-NodeJs-ORM-e-Autenticacao/dia-02-ORM-Associations/para-fixar/src/config/config.js
@@ -1,5 +1,18 @@
 require('dotenv').config();
 
+/*
+Para inicializar o banco de teste, rode os seguintes comandos, 
+passando a variável de ambiente que deseja usar (no nosso caso, NODE_ENV=test):
+
+NODE_ENV=test npx sequelize-cli db:create
+NODE_ENV=test npx sequelize-cli db:migrate
+NODE_ENV=test npx sequelize-cli db:seed:all
+
+Caso queira remover o banco para começar novamente, utilize:
+
+NODE_ENV=test npx sequelize-cli db:drop
+*/
+
 module.exports = {
   development: {
     username: process.env.MYSQL_USER,
