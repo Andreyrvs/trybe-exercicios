821954025c2f888472b51e5771196750377ec2ef
Andrey Rannerson Visniewski <cortaesai@gmail.com>
2022-07-29 18:08:59 -0300

    chore: (para-fixar) Type Aliases - type e type union

---
+++ T19C-Back-End/bloco-26-TypeScript/dia-02-Tipagem-Estatica-e-Generics/para-fixar/dist/index.js
@@ -0,0 +1,5 @@
+"use strict";
+function sumAdd(num) {
+    return num.a + num.b;
+}
+console.log('🚀 ~ sumAdd', sumAdd({ a: 2, b: 6 }));
---
+++ T19C-Back-End/bloco-26-TypeScript/dia-02-Tipagem-Estatica-e-Generics/para-fixar/src/index.ts
@@ -0,0 +1,18 @@
+/* eslint-disable no-unused-vars */
+type Bird = {
+  foot: string;
+  head: string;
+}
+
+type Sum = (x: number, y: number) => number
+
+type Address = {
+  street: string;
+  city: string;
+  state: string;
+}
+
+type EstadosFisicos = 'liquido' | 'solido' | 'gasoso';
+type Cpf = '123.567.890-12' | 123456789012
+type Os = 'linux' | 'mac' | 'windows'
+type Vogais = 'a' | 'e' | 'i' | 'o' | 'u'
