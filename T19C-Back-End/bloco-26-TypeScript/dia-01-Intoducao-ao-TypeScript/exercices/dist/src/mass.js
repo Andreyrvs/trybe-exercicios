d92ae1185678fb7d930a4f93a14085e2b29b2ec0
Andrey Rannerson Visniewski <cortaesai@gmail.com>
2022-07-29 15:30:05 -0300

    chore: Exercicio 06 com gabarito - converte unidades de massa

---
+++ T19C-Back-End/bloco-26-TypeScript/dia-01-Intoducao-ao-TypeScript/exercices/dist/src/mass.js
@@ -0,0 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.units = void 0;
+exports.units = ["kg", "hg", "dag", "g", "dg", "cg", "mg"];
+function convert(value, fromUnit, toUnit) {
+    const fromIndex = exports.units.indexOf(fromUnit);
+    const toIndex = exports.units.indexOf(toUnit);
+    const exponent = (toIndex - fromIndex);
+    return value * Math.pow(10, exponent);
+}
+console.log("🚀 ~ convert", convert(10, "kg", "cg"));
