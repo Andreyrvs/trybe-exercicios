88b5d720360cb572674da43297b7d12522484e14
Andrey Rannerson Visniewski <cortaesai@gmail.com>
2022-08-03 19:26:49 -0300

    chore: (Production)

--- T19C-Back-End/bloco-26-TypeScript/dia-03-Express-com-TypeScript/exercices/dist/app.js
+++ T19C-Back-End/bloco-26-TypeScript/dia-03-Express-com-TypeScript/exercices/dist/app.js
@@ -3,10 +3,10 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+/* eslint-disable no-console */
 const express_1 = __importDefault(require("express"));
-const http_status_codes_1 = require("http-status-codes");
+const User_routes_1 = __importDefault(require("./routes/User.routes"));
 const app = (0, express_1.default)();
 app.use(express_1.default.json());
-const PORT = 3001;
-app.get('/', (req, res) => res.status(http_status_codes_1.StatusCodes.OK).send('Hello World!'));
-app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`));
+app.use('/users', User_routes_1.default);
+exports.default = app;
