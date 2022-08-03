5d01825def86e675756cb8cdda9d88ef8567fd2d
Andrey Rannerson Visniewski <cortaesai@gmail.com>
2022-08-02 21:52:40 -0300

    erro na rota get

--- T19C-Back-End/bloco-26-TypeScript/dia-03-Express-com-TypeScript/express-typescript/dist/models/book.model.js
+++ T19C-Back-End/bloco-26-TypeScript/dia-03-Express-com-TypeScript/express-typescript/dist/models/book.model.js
@@ -1,4 +1,5 @@
 "use strict";
+// ./models/book.model.ts
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
@@ -16,7 +17,7 @@ class BookModel {
     getAll() {
         return __awaiter(this, void 0, void 0, function* () {
             const result = yield this.connection
-                .execute('SELECT * FROM books_api.books');
+                .execute('SELECT * FROM books');
             const [rows] = result;
             return rows;
         });
@@ -24,7 +25,7 @@ class BookModel {
     create(book) {
         return __awaiter(this, void 0, void 0, function* () {
             const { title, price, author, isbn } = book;
-            const result = yield this.connection.execute('INSERT INTO books_api.books (title, price, author, isbn) VALUES (?, ?, ?, ?)', [title, price, author, isbn]);
+            const result = yield this.connection.execute('INSERT INTO books (title, price, author, isbn) VALUES (?, ?, ?, ?)', [title, price, author, isbn]);
             const [dataInserted] = result;
             const { insertId } = dataInserted;
             return Object.assign({ id: insertId }, book);
