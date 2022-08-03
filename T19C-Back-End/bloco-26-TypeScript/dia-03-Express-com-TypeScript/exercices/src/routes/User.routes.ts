2a8f9655f4814480504b1b95e282de6e1f30b3dc
Andrey Rannerson Visniewski <cortaesai@gmail.com>
2022-08-03 20:18:01 -0300

    chore: exercicio 01 (Routes) - Adiciona rota para criar usuarios

--- T19C-Back-End/bloco-26-TypeScript/dia-03-Express-com-TypeScript/exercices/src/routes/User.routes.ts
+++ T19C-Back-End/bloco-26-TypeScript/dia-03-Express-com-TypeScript/exercices/src/routes/User.routes.ts
@@ -7,5 +7,6 @@ const userController = new UserController();
 
 router.get('/', userController.getAll);
 router.get('/:id', userController.getById);
+router.post('/', userController.create);
 
 export default router;
