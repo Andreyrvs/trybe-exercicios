b7129f69a0e8766ebcd3aea2c6b8c886e48dd1bb
Andrey Rannerson Visniewski <cortaesai@gmail.com>
2022-08-03 21:44:03 -0300

    feat: Adiciona validacao do body

---
+++ T19C-Back-End/bloco-26-TypeScript/dia-03-Express-com-TypeScript/exercices/src/validations/userValidation.ts
@@ -0,0 +1,29 @@
+import Joi, { ValidationResult } from 'joi';
+import IUser from '../interfaces/IUser';
+
+const validateUser = (data: IUser): ValidationResult => {
+  const schema = Joi.object({
+    email: Joi.string().email().required()
+      .messages(
+        { 'any.required': 'Email válido é obrigatório' },
+      ),
+    nome: Joi.string().required().min(3)
+      .messages(
+        { 'string.min': 'Nome deve ter no mínimo 3 caracteres' },
+      ),
+    senha: Joi.string().min(6).max(12).required()
+      .messages(
+        {
+          'string.min': 'Password deve ter entre 3 e 12 caracteres',
+        },
+      ),
+  });
+
+  const { error, value } = schema.validate(data);
+
+  if (error) throw error;
+
+  return value;
+};
+
+export default validateUser;
