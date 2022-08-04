ed2e8ce00b6e50370f349487fd9dcb9dba463135
Andrey Rannerson Visniewski <cortaesai@gmail.com>
2022-08-03 21:37:24 -0300

    fix: (Models) - altera todas func para arrow functions e corrige retorno das mesmas

--- T19C-Back-End/bloco-26-TypeScript/dia-03-Express-com-TypeScript/exercices/src/models/User.model.ts
+++ T19C-Back-End/bloco-26-TypeScript/dia-03-Express-com-TypeScript/exercices/src/models/User.model.ts
@@ -1,8 +1,5 @@
 /* eslint-disable no-console */
-import {
-  Pool,
-  ResultSetHeader,
-} from 'mysql2/promise';
+import { Pool, ResultSetHeader } from 'mysql2/promise';
 import IUser from '../interfaces/IUser';
 
 class UserModel {
@@ -12,36 +9,74 @@ class UserModel {
     this.connection = connection;
   }
 
-  public async getAll(): Promise<IUser[]> {
+  public getAll = async (): Promise<IUser[]> => {
     const query = 'SELECT * FROM TypeScriptExpress.Users;';
-    const result = await this.connection.execute(query);
+    const [result] = await this.connection.execute(query);
 
-    const [rows] = result;
-    return rows as IUser[];
-  }
+    return result as IUser[];
+  };
 
-  public async getById(id: number): Promise<IUser[]> {
+  public getById = async (id: number): Promise<IUser | null> => {
     const query = 'SELECT * FROM TypeScriptExpress.Users WHERE id=?';
     const values = [id];
 
-    const result = await this.connection.execute(query, values);
+    const [result] = await this.connection.execute(query, values);
 
-    const [rows] = result;
-    return rows as IUser[];
-  }
+    const [user] = result as IUser[];
+    return user || null;
+  };
 
-  public async create(user: IUser): Promise<IUser> {
+  public getByEmail = async (email: string): Promise<IUser | null> => {
+    const query = 'SELECT * FROM Users WHERE email = ?';
+    const values = [email];
+
+    const [data] = await this.connection.execute(query, values);
+    const [user] = data as IUser[];
+
+    return user || null;
+  };
+
+  public create = async (user: IUser): Promise<IUser> => {
     const { name, email, password } = user;
     const query = 'INSERT INTO TypeScriptExpress.Users (name, email, password) VALUES(?, ?, ?)';
     const values = [name, email, password];
 
-    const result = await this.connection.execute<ResultSetHeader>(query, values);
+    const [result] = await this.connection.execute<ResultSetHeader>(query, values);
+    const { insertId: id } = result;
 
-    const [dataInserted] = result;
-    const { insertId } = dataInserted;
+    const newUser: IUser = {
+      id, name, email, password,
+    };
+    return newUser;
+  };
 
-    return { id: insertId, ...user };
-  }
+  public update = async (id: number, user: IUser): Promise<IUser> => {
+    const { name, email, password } = user;
+
+    const query = 'UPDATE TypeScriptExpress.Users SET name=?, email=?, password=? WHERE id=?';
+    const values = [name, email, password, id];
+
+    await this.connection.execute(query, values);
+
+    const editedUser: IUser = {
+      id, name, email, password,
+    };
+
+    return editedUser;
+  };
+
+  public remove = async (id:number): Promise<IUser | null> => {
+    const userToBeDeleted = await this.getById(id);
+
+    if (!userToBeDeleted) return null;
+
+    const query = 'DELETE FROM TypeScriptExpress.Users WHERE id=?';
+    const values = [id];
+
+    await this.connection.execute(query, values);
+
+    return userToBeDeleted;
+  };
 }
 
 export default UserModel;
