f74ff052e05d851a2c0e9aa9c0651fe9fe066242
Andrey Rannerson Visniewski <cortaesai@gmail.com>
2022-08-03 22:33:00 -0300

    Exercicio 02

--- T19C-Back-End/bloco-26-TypeScript/dia-03-Express-com-TypeScript/exercices/src/app.ts
+++ T19C-Back-End/bloco-26-TypeScript/dia-03-Express-com-TypeScript/exercices/src/app.ts
@@ -1,10 +1,12 @@
 /* eslint-disable no-console */
 import express from 'express';
 import UserRoutes from './routes/user.routes';
+import PostRoutes from './routes/post.routes';
 
 const app = express();
 
 app.use(express.json());
 app.use('/users', UserRoutes);
+app.use('/posts', PostRoutes);
 
 export default app;
