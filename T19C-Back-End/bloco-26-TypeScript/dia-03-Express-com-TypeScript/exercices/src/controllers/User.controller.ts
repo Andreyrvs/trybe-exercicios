eea88f7f8f7c28d8fcc2bbfbb6254642c5c7d0be
Andrey Rannerson Visniewski <cortaesai@gmail.com>
2022-08-03 21:34:55 -0300

    fix: (Controllers) - Qualidade de codigo. Utiliza bloco try/catch e usa next middleware.

--- T19C-Back-End/bloco-26-TypeScript/dia-03-Express-com-TypeScript/exercices/src/controllers/User.controller.ts
+++ T19C-Back-End/bloco-26-TypeScript/dia-03-Express-com-TypeScript/exercices/src/controllers/User.controller.ts
@@ -1,32 +1,63 @@
-import { Request, Response } from 'express';
-import { StatusCodes } from 'http-status-codes';
+/* eslint-disable consistent-return */
+import { Request, Response, NextFunction } from 'express';
+import HTTP_STATUS from '../enums/HttpStatus';
+import IUser from '../interfaces/IUser';
 import UserService from '../services/User.service';
 
 class UserController {
   constructor(private userService = new UserService()) { }
 
-  public getAll = async (req: Request, res:Response) => {
+  public getAll = async (_req: Request, res:Response) => {
     const result = await this.userService.getAll();
 
-    return res.status(StatusCodes.OK).json(result);
+    return res.status(HTTP_STATUS.OK).json(result);
   };
 
-  public getById = async (req: Request, res: Response) => {
-    const id = Number(req.params.id);
-    const result = await this.userService.getById(id);
+  public getById = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const { id } = req.params;
+      const result = await this.userService.getById(Number(id));
 
-    return res.status(StatusCodes.OK).json(result);
-  };
-
-  public create = async (req: Request, res: Response) => {
-    const user = req.body;
-
-    const result = await this.userService.create(user);
-
-    if (!result) {
-      return res.status(StatusCodes.BAD_GATEWAY).send('BadRequest');
+      return res.status(HTTP_STATUS.OK).json(result);
+    } catch (error) {
+      next(error);
     }
-    return res.status(StatusCodes.OK).json(result);
+  };
+
+  public create = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const user = req.body as IUser;
+
+      const result = await this.userService.create(user);
+      return res.status(HTTP_STATUS.CREATED).json(result);
+    } catch (error) {
+      next(error);
+    }
+  };
+
+  public update = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const { id } = req.params;
+      const user = req.body as IUser;
+
+      const result = await this.userService.update(Number(id), user);
+
+      return res.status(HTTP_STATUS.CREATED).json(result);
+    } catch (error) {
+      next(error);
+    }
+  };
+
+  public remove = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const { id } = req.params;
+
+      const result = await this.userService.remove(Number(id));
+
+      return res.status(HTTP_STATUS.OK).json(result);
+    } catch (error) {
+      next(error);
+    }
   };
 }
 
