fbef5df7c731802711433bbf1e775fee8c9d6c9d
Andrey Rannerson Visniewski <cortaesai@gmail.com>
2022-08-08 11:03:37 -0300

    chore: (para-fixar) - Sintaxe geral POO

---
+++ T19C-Back-End/bloco-27-Programacao-orientada-a-Objetos-POO-e-SOLID/dia-01-Introducao-a-Orientacao-a-Objetos/para-fixar/dist/index.js
@@ -0,0 +1,15 @@
+"use strict";
+class Tv {
+    constructor(brand, size, resolution, connections, connectedTo) {
+        this.brand = brand;
+        this.size = size;
+        this.resolution = resolution;
+        this.connections = connections;
+        this.connectedTo = connectedTo;
+    }
+    turnOn() {
+        console.log(`Marca da TV: ${this.brand}, Tamanho em polegadas: ${this.size}in, Resolução da tela: ${this.resolution}, conexões disponíveis(HDMI, Ethernet, etc.):${this.connections}`);
+    }
+}
+const tv1 = new Tv('Tochiba', 22, 'Full hd', 'HDMI, P2', "?");
+tv1.turnOn();
---
+++ T19C-Back-End/bloco-27-Programacao-orientada-a-Objetos-POO-e-SOLID/dia-01-Introducao-a-Orientacao-a-Objetos/para-fixar/index.ts
@@ -0,0 +1,24 @@
+class Tv {
+  brand:string;
+  size: number;
+  resolution: string;
+  connections: string;
+  connectedTo: string;
+
+  constructor(brand: string, size: number, resolution: string, connections: string, connectedTo:string) {
+    this.brand = brand;
+    this.size = size;
+    this.resolution = resolution;
+    this.connections = connections;
+    this.connectedTo = connectedTo
+  }
+
+  turnOn() {
+    console.log(`Marca da TV: ${this.brand}, Tamanho em polegadas: ${this.size}in, Resolução da tela: ${this.resolution}, conexões disponíveis(HDMI, Ethernet, etc.):${this.connections}`);
+    
+  }
+}
+
+const tv1 = new Tv('Tochiba', 22, 'Full hd','HDMI, P2', "?")
+
+tv1.turnOn()
