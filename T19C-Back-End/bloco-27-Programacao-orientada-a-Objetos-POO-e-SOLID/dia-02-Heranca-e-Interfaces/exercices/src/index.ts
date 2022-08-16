8cebe21e02f5dd57ef777147f1855f844d7dc390
Andrey Rannerson Visniewski <cortaesai@gmail.com>
2022-08-16 20:49:51 -0300

    chore: exercice 03

--- T19C-Back-End/bloco-27-Programacao-orientada-a-Objetos-POO-e-SOLID/dia-02-Heranca-e-Interfaces/exercices/src/index.ts
+++ T19C-Back-End/bloco-27-Programacao-orientada-a-Objetos-POO-e-SOLID/dia-02-Heranca-e-Interfaces/exercices/src/index.ts
@@ -1,13 +1,16 @@
 // import Person from './Person';
 import Student from './Student'
+import Employee from './interfaces/Employee';
 
-// const andrey = new Person('andrey', new Date('1997/06/16'))
-// const junin = new Person('junin', new Date('2000/02/10'))
+// Exercicio 01
+/* const andrey = new Person('andrey', new Date('1997/06/16'))
+const junin = new Person('junin', new Date('2000/02/10'))
 
-// console.log(andrey);
-// console.log(junin);
+console.log(andrey);
+console.log(junin); */
 
-const andrey = new Student('Andrey', new Date('1997/06/16'));
+// Exercicio 02
+/* const andrey = new Student('Andrey', new Date('1997/06/16'));
 const carolina = new Student('Carolina da Silva', new Date('2005/03/17'));
 const lucas = new Student('Lucas Peixoto Salgueiro', new Date('2006/07/19'));
 const jessica = new Student('Jéssica Bianca Nunes', new Date('2004/06/06'));
@@ -26,4 +29,18 @@ console.log(carolina);
 console.log(lucas);
 console.log(jessica);
 console.log(tamires);
-console.log(fernando);
+console.log(fernando); */
+
+// Exercicio 03
+/* const testInterfaceEmployee: Employee = {
+  registration: 'FNC1234567891011',
+  salary: 1200.00,
+  admissionDate: new Date(),
+
+  generateRegistration(): string {
+    const randomStr = String(Date.now() * (Math.random() + 1)).replace(/\W/g, '')
+    return `FNC${randomStr}`
+  },
+}
+console.log("🚀 ~ testInterfaceEmployee", testInterfaceEmployee)
+ */
