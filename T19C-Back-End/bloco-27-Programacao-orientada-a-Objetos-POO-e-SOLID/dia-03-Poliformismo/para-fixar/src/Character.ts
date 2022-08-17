e15fe119cba96339389fec22af92795262d15b11
Andrey Rannerson Visniewski <cortaesai@gmail.com>
2022-08-17 17:38:10 -0300

    chore: (para-fixar) - Poliformismo 02

--- T19C-Back-End/bloco-27-Programacao-orientada-a-Objetos-POO-e-SOLID/dia-03-Poliformismo/para-fixar/src/Character.ts
+++ T19C-Back-End/bloco-27-Programacao-orientada-a-Objetos-POO-e-SOLID/dia-03-Poliformismo/para-fixar/src/Character.ts
@@ -18,15 +18,39 @@ interface IModel {
 const db: DbCharacter[] = [];
 
 class LocalDbModel implements IModel {
-  create: (character: Character) => Promise<DbCharacter>;
-  update: (id: number, character: Character) => Promise<DbCharacter>;
-  delete: (id: number) => Promise<Boolean>;
-  getAll: () => Promise<DbCharacter[]>;
-  getById: (id: number) => Promise<DbCharacter>;
+  create = async (character: Character) => {
+    const lastId = db.length > 0 ? db[db.length - 1].id : 0;
+    const newCharacter = { id: lastId + 1, ...character };
+    db.push(newCharacter);
+    return newCharacter
+  }
 
+  findIndexById = (id: number) => {
+    const index = db.findIndex((character) => character.id === id);
+    if (index < 0 ) {
+      throw new Error("Character not found");
+    }
+    return index;
+  }
 
-  constructor(parameters) {
-    
+  update = async (id: number, character: Character) => {
+    const indexToUpdate = this.findIndexById(id)
+    db[indexToUpdate] = { ...db[indexToUpdate], ...character };
+    return db[indexToUpdate]
+  }
+
+  delete = async (id: number) => {
+    const indexToDelete = this.findIndexById(id);
+    const deletedItem = db.splice(indexToDelete, 1);
+    if (deletedItem.length > 0) return true;
+    return false;
+  }
+
+  getAll = async () => db;
+
+  getById = async (id: number) => {
+    const indexToGet = this.findIndexById(id);
+    return db[indexToGet]
   }
 }
 
@@ -37,10 +61,12 @@ class CharacterService {
 }
 
 class MockedDbModel implements IModel {
-  create: void;
-  read: void;
-  update: void;
-  delete: void;
+  create: (character: Character) => Promise<DbCharacter>;
+  update: (id: number, character: Character) => Promise<DbCharacter>;
+  delete: (id: number) => Promise<Boolean>;
+  getAll: () => Promise<DbCharacter[]>;
+  getById: (id: number) => Promise<DbCharacter>;
+
   constructor(parameters) {
     
   }
