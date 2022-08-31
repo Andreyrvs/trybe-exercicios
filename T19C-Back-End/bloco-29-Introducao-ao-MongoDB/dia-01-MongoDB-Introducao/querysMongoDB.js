016ebe482bf8d7466e125c65902036b1efc92eb9
Andrey Rannerson Visniewski <cortaesai@gmail.com>
2022-08-30 21:31:01 -0300

    chore: Exerciios 07

--- T19C-Back-End/bloco-29-Introducao-ao-MongoDB/dia-01-MongoDB-Introducao/querysMongoDB.js
+++ T19C-Back-End/bloco-29-Introducao-ao-MongoDB/dia-01-MongoDB-Introducao/querysMongoDB.js
@@ -15,3 +15,14 @@ db.bios.find({"name.first": "John"}).pretty()
 
 // Exercicio 06
 db.bios.find({}).limit(2).skip(5)
+
+// Exercicio 07 
+// docker cp books.json 01871965c939:/tmp/books.json  
+// docker exec 01871965c939 mongoimport -d sample -c books --file /tmp/books.json
+db.books.countDocuments({})
+
+// Exercicio 08 
+
+// Exercicio 09 
+
+// Exercicio 10 
