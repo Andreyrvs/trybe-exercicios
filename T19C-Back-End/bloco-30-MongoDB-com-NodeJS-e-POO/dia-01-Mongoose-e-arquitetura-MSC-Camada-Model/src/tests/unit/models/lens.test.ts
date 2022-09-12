2a60d90a2ae88bb17776c958bf8ec9ee558e5dca
Andrey Rannerson Visniewski <cortaesai@gmail.com>
2022-09-12 20:52:19 -0300

    chore: (exercices) 01 - Cria teste para a classe lens

--- T19C-Back-End/bloco-30-MongoDB-com-NodeJS-e-POO/dia-01-Mongoose-e-arquitetura-MSC-Camada-Model/src/tests/unit/models/lens.test.ts
+++ T19C-Back-End/bloco-30-MongoDB-com-NodeJS-e-POO/dia-01-Mongoose-e-arquitetura-MSC-Camada-Model/src/tests/unit/models/lens.test.ts
@@ -5,5 +5,21 @@ import { Model } from 'mongoose';
 import { lensMock, lensMockWithId } from '../../mocks/lensMock';
 
 describe('Lens Model', () => {
-  
+  const lensModel = new LensModel();
+
+  before(()=> {
+    sinon.stub(Model, 'create').resolves(lensMockWithId);
+    // sinon.stub(Model, 'findOne').resolves(lensMockWithId);
+  });
+
+  after(()=>{
+    sinon.restore();
+  });
+
+  describe('creating a frame', () => {
+    it('successfully created',async () => {
+      const newLens = await lensModel.create(lensMock)
+      expect(newLens).to.be.deep.equal(lensMockWithId)
+    })
+  })
 })
