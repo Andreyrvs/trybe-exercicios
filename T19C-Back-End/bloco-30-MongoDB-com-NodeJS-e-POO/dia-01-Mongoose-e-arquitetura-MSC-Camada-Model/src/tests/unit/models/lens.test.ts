2ae4abe062303b662b55ef596a0a16477e710bd4
Andrey Rannerson Visniewski <cortaesai@gmail.com>
2022-09-12 20:48:18 -0300

    chore: Para fixar

---
+++ T19C-Back-End/bloco-30-MongoDB-com-NodeJS-e-POO/dia-01-Mongoose-e-arquitetura-MSC-Camada-Model/src/tests/unit/models/lens.test.ts
@@ -0,0 +1,9 @@
+import { expect } from 'chai';
+import sinon from 'sinon';
+import LensModel from '../../../models/Lens';
+import { Model } from 'mongoose';
+import { lensMock, lensMockWithId } from '../../mocks/lensMock';
+
+describe('Lens Model', () => {
+  
+})
