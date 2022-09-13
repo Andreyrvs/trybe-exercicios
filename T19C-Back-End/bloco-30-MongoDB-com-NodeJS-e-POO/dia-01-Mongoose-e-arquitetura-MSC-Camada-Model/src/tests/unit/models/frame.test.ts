1a70f2934f30317549013c1f293a07bb1f12c0f8
Andrey Rannerson Visniewski <cortaesai@gmail.com>
2022-09-12 21:15:24 -0300

    chore: (exercices) 04 - faz o test do read da classe Frame

--- T19C-Back-End/bloco-30-MongoDB-com-NodeJS-e-POO/dia-01-Mongoose-e-arquitetura-MSC-Camada-Model/src/tests/unit/models/frame.test.ts
+++ T19C-Back-End/bloco-30-MongoDB-com-NodeJS-e-POO/dia-01-Mongoose-e-arquitetura-MSC-Camada-Model/src/tests/unit/models/frame.test.ts
@@ -3,13 +3,16 @@ import sinon from 'sinon';
 import FrameModel from '../../../models/Frame';
 import { Model } from 'mongoose';
 import { frameMock, frameMockWithId } from '../../mocks/frameMock';
+import IFrame from '../../../interfaces/Frame';
 
 describe('Frame Model', () => {
   const frameModel = new FrameModel();
+  const frameList = [frameMockWithId]
 
   before(() => {
     sinon.stub(Model, 'create').resolves(frameMockWithId);
     sinon.stub(Model, 'findOne').resolves(frameMockWithId);
+    sinon.stub(Model, 'find').resolves(frameList);
   })
 
   after(() => {
@@ -32,6 +35,24 @@ describe('Frame Model', () => {
     it('_id not found', async () => {
       try {
         await frameModel.readOne('123ERRADO');
+      } catch (error: any) {
+        expect(error.message).to.be.eq('InvalidMongoId');
+      }
+    });
+  });
+
+  describe('searching frames', () => {
+    it('successfully found', async () => {
+      const framesFound = await frameModel.read();
+      expect(framesFound).to.be.an('array');
+      framesFound?.forEach((frame: IFrame, index: number) => {
+        expect(frame).to.be.deep.equal(frameList[index])
+      });
+    })
+
+    it('_id not found', async () => {
+      try {
+        await frameModel.read();
       } catch (error: any) {
         expect(error.message).to.be.eq('InvalidMongoId');
       }
