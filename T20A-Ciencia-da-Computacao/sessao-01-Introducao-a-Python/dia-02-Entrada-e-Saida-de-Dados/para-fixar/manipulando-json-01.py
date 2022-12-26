e5b9235fb70e52343ccf93af6ad17348fe6dc355
Andrey Visniewski <cortaesai@gmail.com>
2022-12-26 11:37:47 -0300

    chore: Manipulando arquivos Json - Metodo equivalente para escrever de forma direta

--- T20A-Ciencia-da-Computacao/sessao-01-Introducao-a-Python/dia-02-Entrada-e-Saida-de-Dados/para-fixar/manipulando-json-01.py
+++ T20A-Ciencia-da-Computacao/sessao-01-Introducao-a-Python/dia-02-Entrada-e-Saida-de-Dados/para-fixar/manipulando-json-01.py
@@ -7,8 +7,13 @@ grass_type_pokemons = [
     pokemon for pokemon in pokemons if "Grass" in pokemon["type"]
 ]
 
+# with open("grass_pokemons.json", "w") as file:
+#     json_to_write = json.dumps(
+#         grass_type_pokemons
+#     )
+#     file.write(json_to_write)
+
+# método equivalente para escrever em arquivos de forma direta.
+
 with open("grass_pokemons.json", "w") as file:
-    json_to_write = json.dumps(
-        grass_type_pokemons
-    )
-    file.write(json_to_write)
+    json.dump(grass_type_pokemons, file)
