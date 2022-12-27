b57f432093c88043da9842523ed79aec579822be
Andrey Visniewski <cortaesai@gmail.com>
2022-12-27 19:14:30 -0300

    Testes

--- T20A-Ciencia-da-Computacao/sessao-01-Introducao-a-Python/dia-03-Testes/para-fixar/test_codigo.py
+++ T20A-Ciencia-da-Computacao/sessao-01-Introducao-a-Python/dia-03-Testes/para-fixar/test_codigo.py
@@ -1,4 +1,5 @@
-from codigo import is_odd
+import pytest
+from codigo import is_odd, divide
 
 def test_is_odd_when_number_is_ood_return_true():
     'Para um número ímpar a função deve retornar o valor True'
@@ -8,4 +9,9 @@ def test_is_odd_when_number_is_ood_return_true():
 def test_is_odd_when_number_is_ood_return_false():
     'Para um número par a função deve retornar o valor False'
     assert is_odd(2) is False
+
+
+def test_divide_when_other_number_is_zero_raises_an_exception():
+    with pytest.raises(ZeroDivisionError, match="division by zero"):
+        divide(2, 0)
 
